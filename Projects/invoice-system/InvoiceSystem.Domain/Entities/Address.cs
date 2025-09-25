82e1b2bea1fb3d70bce2ad69a20e8a089e2282c9
zulhamzakaria <m.zulham.zakaria@gmail.com>
2025-09-25 08:24:23 +0800

    - Address as value object

--- Projects/invoice-system/InvoiceSystem.Domain/Entities/Address.cs
+++ Projects/invoice-system/InvoiceSystem.Domain/Entities/Address.cs
@@ -7,18 +7,24 @@ public class Address
     public string State { get; private set; }
     public string ZipCode { get; private set; }
     public string Country { get; private set; }
+    public AddressType Type { get; set; }
 
     private Address() { } // For EF Core
 
-    public Address(string street, string city, string state, string zipCode, string country)
+    public Address(string street, string city, string state, string zipCode, string country, AddressType type)
     {
         Street = street ?? throw new ArgumentNullException(nameof(street));
         City = city ?? throw new ArgumentNullException(nameof(city));
         State = state ?? throw new ArgumentNullException(nameof(state));
         ZipCode = zipCode ?? throw new ArgumentNullException(nameof(zipCode));
         Country = country ?? throw new ArgumentNullException(nameof(country));
+        Type = type;
     }
+}
 
-
-
+public enum AddressType
+{
+    HQ,
+    Billing,
+    Shipping
 }
