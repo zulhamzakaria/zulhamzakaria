1777a75c4c28b273d976156058b434c079fcb39d
zulhamzakaria <m.zulham.zakaria@gmail.com>
2025-09-30 09:30:06 +0800

    - upgraded DomainException - nullability is a vast topic?

--- Projects/invoice-system/InvoiceSystem.Domain/Common/DomainException.cs
+++ Projects/invoice-system/InvoiceSystem.Domain/Common/DomainException.cs
@@ -1,7 +1,18 @@
 ﻿namespace InvoiceSystem.Domain.Common;
 
-public class DomainException : Exception
+public class DomainException : InvalidOperationException
 {
-    public DomainException(string message) : base(message) { }
-
+    public string? ErrorCode { get; }
+    public IReadOnlyDictionary<string, object> DataContext { get; }
+    public DomainException(string message, string errorCode, IReadOnlyDictionary<string,object>? dataContext) : base(message) {
+        if (string.IsNullOrWhiteSpace(message)) 
+            throw new ArgumentNullException(nameof(message));
+        if (string.IsNullOrWhiteSpace(errorCode)) 
+            throw new ArgumentNullException(nameof(errorCode));
+        ErrorCode = errorCode;
+        DataContext = dataContext is not null ? 
+                        new Dictionary<string, object>(dataContext) : 
+                        new Dictionary<string, object>();
+    }
+    public DomainException(string message, string errorCode) : this(message, errorCode, null) {}
 }
