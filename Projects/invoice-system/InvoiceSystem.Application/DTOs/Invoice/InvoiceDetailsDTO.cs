bd6f9180a7c2cc39cbeb3306ec6b442fb0b43ac3
zulhamzakaria <m.zulham.zakaria@gmail.com>
2025-10-09 21:32:48 +0800

    - GETInvoiceDTO. use DTO for getting the data instead of referring to the Entity (AddressDTO vs Address)

--- Projects/invoice-system/InvoiceSystem.Application/DTOs/Invoice/InvoiceDetailsDTO.cs
+++ Projects/invoice-system/InvoiceSystem.Application/DTOs/Invoice/InvoiceDetailsDTO.cs
@@ -2,4 +2,12 @@
 
 namespace InvoiceSystem.Application.DTOs.Invoice;
 
-public record InvoiceDetailsDTO (Guid Id, string InvoiceNo, Company Company, Address BillingAddress, Address ShippingAddress, DateTime InvoiceDate, List<InvoiceItem> InvoiceItems);
+public record InvoiceDetailsDTO (Guid Id, 
+                                string InvoiceNo, 
+                                Company Company, 
+                                Address BillingAddress, 
+                                Address ShippingAddress, 
+                                DateTime InvoiceDate, 
+                                decimal InvoiceAmount,
+                                string Status,
+                                List<InvoiceItem> InvoiceItems);
