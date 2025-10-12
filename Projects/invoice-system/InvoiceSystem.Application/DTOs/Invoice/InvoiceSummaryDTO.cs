4e694251d91e4d358553cd673b8313f6d5e61c0f
zulhamzakaria <m.zulham.zakaria@gmail.com>
2025-10-12 23:23:54 +0800

    - InvoiceSummaryDTO

---
+++ Projects/invoice-system/InvoiceSystem.Application/DTOs/Invoice/InvoiceSummaryDTO.cs
@@ -0,0 +1,12 @@
+﻿using InvoiceSystem.Application.DTOs.Company;
+
+namespace InvoiceSystem.Application.DTOs.Invoice;
+
+public record InvoiceSummaryDTO (
+    Guid Id,
+    string InvoiceNo,
+    DateTime InvoiceDate,
+    decimal InvoiceAmount,
+    string Status,
+    CompanySummaryDTO Company,
+    int TotalItemsCount);
