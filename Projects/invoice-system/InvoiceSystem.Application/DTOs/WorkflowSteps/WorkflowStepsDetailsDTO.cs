a89a3ab14fe36d1fb989e2754719d2fca8cac43b
zulhamzakaria <m.zulham.zakaria@gmail.com>
2025-10-13 04:51:26 +0800

    - WorkflowstepsDetailsDTO

---
+++ Projects/invoice-system/InvoiceSystem.Application/DTOs/WorkflowSteps/WorkflowStepsDetailsDTO.cs
@@ -0,0 +1,12 @@
+﻿namespace InvoiceSystem.Application.DTOs.WorkflowSteps;
+
+public record WorkflowStepsDetailsDTO(
+    Guid Id,
+    Guid InvoiceId,
+    string StatusBefore,
+    string StatusAfter,
+    string ActionType,
+    Guid ApproverId,
+    string Reason,
+    DateTimeOffset Timestamp
+    );
