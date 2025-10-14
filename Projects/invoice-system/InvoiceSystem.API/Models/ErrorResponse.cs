555c330666dcba4811189036aa84a5e2554262c9
zulhamzakaria <m.zulham.zakaria@gmail.com>
2025-10-14 19:03:48 +0800

    - how does this work?

---
+++ Projects/invoice-system/InvoiceSystem.API/Models/ErrorResponse.cs
@@ -0,0 +1,17 @@
+﻿using InvoiceSystem.Domain.Common;
+
+namespace InvoiceSystem.API.Models;
+
+public sealed record ErrorResponse
+{
+    public string Title { get; init; } = "Bad request";
+    public int Status {  get; init; } = 400;
+    public IReadOnlyList<ErrorDetail> Errors { get; init; }
+
+    public ErrorResponse(IReadOnlyList<Error> errors)
+    {
+        Errors = errors.Select(e => new ErrorDetail(e.Code, e.Message)).ToList();
+    }
+}
+
+public sealed record ErrorDetail(string Code, string Message);
--- Projects/invoice-system/InvoiceSystem.API/obj/Debug/net8.0/InvoiceSystem.API.AssemblyInfo.cs
+++ Projects/invoice-system/InvoiceSystem.API/obj/Debug/net8.0/InvoiceSystem.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("InvoiceSystem.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+94cc2b0ad35771ea568eb47bdd300824a5bb2a84")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2a59c0a65f54b85a11657be34fe43d3a1f64e19e")]
 [assembly: System.Reflection.AssemblyProductAttribute("InvoiceSystem.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("InvoiceSystem.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
