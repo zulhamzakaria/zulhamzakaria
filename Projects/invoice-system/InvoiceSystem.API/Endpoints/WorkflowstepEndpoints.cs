2a59c0a65f54b85a11657be34fe43d3a1f64e19e
zulhamzakaria <m.zulham.zakaria@gmail.com>
2025-10-14 18:55:57 +0800

    - WIP

---
+++ Projects/invoice-system/InvoiceSystem.API/Endpoints/WorkflowstepEndpoints.cs
@@ -0,0 +1,46 @@
+﻿using InvoiceSystem.Application.DTOs.WorkflowSteps;
+using InvoiceSystem.Application.Services.Interfaces;
+using InvoiceSystem.Domain.Common;
+using System.Net.Mime;
+using System.Threading.Tasks;
+
+namespace InvoiceSystem.API.Endpoints;
+
+public static class WorkflowstepEndpoints
+{
+
+    public static void MapWorkflowstepEndpoints(this IEndpointRouteBuilder builder)
+    {
+        var group = builder.MapGroup("api/workflowsteps").WithTags("Workflow Steps");
+        group.MapPost("/", CreateWorkflowStep)
+           .WithName("CreateWorkflowStep")
+           .Produces<WorkflowstepsDetailsDTO>(StatusCodes.Status201Created, MediaTypeNames.Application.Json)
+           .Produces<ErrorResponse>(StatusCodes.Status400BadRequest, MediaTypeNames.Application.Json)
+           .WithOpenApi();
+
+        // --- GET: Get Workflow Step Details (A placeholder Query) ---
+        group.MapGet("/{id:guid}", GetWorkflowstepDetails)
+            .WithName("GetWorkflowStepDetails")
+            .Produces<WorkflowstepsDetailsDTO>(StatusCodes.Status200OK, MediaTypeNames.Application.Json)
+            .Produces(StatusCodes.Status404NotFound)
+            .WithOpenApi();
+
+    }
+
+    private static async Task<IResult> CreateWorkflowStep(WorkflowstepsCreationDTO dto, IWorkflowstepService service)
+    {
+        var result = await service.CreateWorkflowstepAsync(dto);
+        if (result.IsSuccess) { return Results.Created($"/api/workflowsteps/{result.Value.Id}", result.Value); }
+
+        return Results.BadRequest();
+
+    }
+
+    private static async Task<IResult> GetWorkflowstepDetails(Guid id, IWorkflowstepService service)
+    {
+        // not implemented yet. night not need it
+        //var result=  await service.
+        return Results.NotFound($"Workflow step with ID {id} not found (handler is a placeholder).");
+    }
+
+}
--- Projects/invoice-system/InvoiceSystem.API/obj/Debug/net8.0/InvoiceSystem.API.AssemblyInfo.cs
+++ Projects/invoice-system/InvoiceSystem.API/obj/Debug/net8.0/InvoiceSystem.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("InvoiceSystem.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e60bd40aaedaf30d987b3cc2ff181d8d176f2054")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+94cc2b0ad35771ea568eb47bdd300824a5bb2a84")]
 [assembly: System.Reflection.AssemblyProductAttribute("InvoiceSystem.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("InvoiceSystem.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
