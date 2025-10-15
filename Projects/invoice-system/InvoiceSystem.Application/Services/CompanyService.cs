82dd1d7f24be8aa74515ce7410c9d02fe97ad5eb
zulhamzakaria <m.zulham.zakaria@gmail.com>
2025-10-15 22:10:48 +0800

    - Enum checker

--- Projects/invoice-system/InvoiceSystem.Application/Services/CompanyService.cs
+++ Projects/invoice-system/InvoiceSystem.Application/Services/CompanyService.cs
@@ -1,6 +1,7 @@
 ﻿using InvoiceSystem.Application.DTOs.Company;
 using InvoiceSystem.Application.Services.Interfaces;
 using InvoiceSystem.Domain.Common;
+using InvoiceSystem.Domain.Entities;
 using InvoiceSystem.Domain.Errors;
 using InvoiceSystem.Domain.Repositories;
 using System.Runtime.CompilerServices;
@@ -23,5 +24,26 @@ public class CompanyService : ICompanyService
             var errors = new List<Error> { Error.Validation(CompanyErrors.Service.CompanyExists, "A company record with the provided unique identifiers already exists") };
         }
 
+        var billingAddress = Address.Create(
+            dto.BillingAddress.Street,
+            dto.BillingAddress.City,
+            dto.BillingAddress.Zipcode,
+            dto.BillingAddress.State,
+            dto.BillingAddress.Country,
+            Enum.TryParse(dto.BillingAddress.AddressType)
+            )
+
     }
+
+    private Result<TEnum> TryConvertStringToEnum<TEnum>(
+        string inputValue, string errorCode, string fieldName, string expectedValues) where TEnum : struct
+    {
+        if (!Enum.TryParse<TEnum>(inputValue, true, out TEnum result))
+        {
+            return Result<TEnum>.Failure(Error.Validation("Invalid enum value",
+                $"The {fieldName} value '{inputValue}' is not valid. Expected values are: {expectedValues}."));
+        }
+        return Result<TEnum>.Success(result);
+    }
+
 }
