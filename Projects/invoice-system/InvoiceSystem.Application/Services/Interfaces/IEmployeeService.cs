a1317a3a7fa215e71caa5a72f5bc32aec9e8a079
zulhamzakaria <m.zulham.zakaria@gmail.com>
2025-10-19 22:31:37 +0800

    - Interface for Employee Service

---
+++ Projects/invoice-system/InvoiceSystem.Application/Services/Interfaces/IEmployeeService.cs
@@ -0,0 +1,14 @@
+﻿using InvoiceSystem.Application.DTOs.Employee;
+using InvoiceSystem.Domain.Common;
+
+namespace InvoiceSystem.Application.Services.Interfaces;
+
+public interface IEmployeeService
+{
+    Task<Result<EmployeeDetailsDTO>> GetEmployeeByIdAsync(int id);
+    Task<Result<List<EmployeeSummaryDTO>>> GetAllEmployeesAsync();
+    Task<Result<EmployeeCreationDTO>> CreateEmployeeAsync(EmployeeCreationDTO employeeCreationDTO);
+    Task<Result<EmployeeUpdateDTO>> UpdateEmployeeAsync(Guid id,  EmployeeUpdateDTO employeeUpdateDTO);
+    Task<Result> DeactivateEmployeeAsync(Guid id); 
+
+}
