2bd0a99699f932f763b761f54b0b3f30a5e85cf5
zulhamzakaria <m.zulham.zakaria@gmail.com>
2025-10-22 19:58:51 +0800

    - ItemDetails mapper. List doesnt require a separate DTO

---
+++ Projects/invoice-system/InvoiceSystem.Application/Mappers/InvoiceItemMapper.cs
@@ -0,0 +1,19 @@
+﻿using InvoiceSystem.Application.DTOs.InvoiceItem;
+using InvoiceSystem.Domain.Entities;
+
+namespace InvoiceSystem.Application.Mappers;
+
+public static class InvoiceItemMapper
+{
+    public static InvoiceItemDTO ToDetailsDTO(InvoiceItem item)
+    {
+        return new InvoiceItemDTO(item.Id, item.Description, item.Quantity, item.UnitPrice, item.TotalPrice);
+    }
+
+    public static InvoiceItemCreationDTO ToCreationDTO(InvoiceItem item)
+    {
+        return new InvoiceItemCreationDTO(item.Description, item.Quantity, item.UnitPrice);
+    }
+
+    public static IReadOnlyList<InvoiceItemDTO> ToListDTO(IEnumerable<InvoiceItem> items) => items.Select(ToDetailsDTO).ToList(); 
+}
