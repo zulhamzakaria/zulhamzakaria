b01abd18ffceb3691902100f103d00a76e96bb85
zulhamzakaria <m.zulham.zakaria@gmail.com>
2025-10-22 19:47:40 +0800

    - Qty 0 doesnt make sense

--- Projects/invoice-system/InvoiceSystem.Application/DTOs/InvoiceItem/InvoiceItemCreationDTO.cs
+++ Projects/invoice-system/InvoiceSystem.Application/DTOs/InvoiceItem/InvoiceItemCreationDTO.cs
@@ -5,7 +5,7 @@ namespace InvoiceSystem.Application.DTOs.InvoiceItem;
 public record InvoiceItemCreationDTO(
     [property: Required, StringLength(500, MinimumLength =1)]
     string Description,
-    [property: Required, Range(0, int.MaxValue)]
+    [property: Required, Range(1, int.MaxValue)]
     int Quantity,
     [property: Required, Range(0.0001, int.MaxValue)]
     decimal UnitPrice
