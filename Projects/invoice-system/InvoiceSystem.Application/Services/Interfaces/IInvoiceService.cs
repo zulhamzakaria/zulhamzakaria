72e97e12740daf4f1a93810d8e968cc3ca83b679
zulhamzakaria <m.zulham.zakaria@gmail.com>
2025-10-23 11:26:59 +0800

    - Service (WIP)

--- Projects/invoice-system/InvoiceSystem.Application/Services/Interfaces/IInvoiceService.cs
+++ Projects/invoice-system/InvoiceSystem.Application/Services/Interfaces/IInvoiceService.cs
@@ -1,5 +1,20 @@
-﻿namespace InvoiceSystem.Application.Services.Interfaces;
+﻿using InvoiceSystem.Application.DTOs.Invoice;
+using InvoiceSystem.Application.DTOs.InvoiceItem;
+using InvoiceSystem.Domain.Common;
+using InvoiceSystem.Domain.Enums;
+
+namespace InvoiceSystem.Application.Services.Interfaces;
 
 public interface IInvoiceService
 {
-}
+    public Task<Result<InvoiceDetailsDTO>> CreateInvoiceAsync(InvoiceCreationDTO creationDTO);
+    public Task<Result<InvoiceDetailsDTO>> GetInvoiceByIdAsync(Guid invoiceId);
+    public Task<Result<IReadOnlyList<InvoiceSummaryDTO>>> GetAllInvoicesAsync();
+
+    public Task<Result> UpdateInvoiceAsync(Guid invoiceId, InvoiceUpdateDTO updateDTO);
+    public Task<Result> VoidInvoiceAsync(Guid invoiceId, EmployeeType employeeType);
+
+
+    public Task<Result<InvoiceItemDTO>> CreateInvoiceItemAsync(Guid invoiceId, InvoiceItemCreationDTO itemDTO);
+    public Task<Result> DeleteInvoiceItemsAsync(Guid invoiceId, Guid itemId, EmployeeType employeeType);
+ }
--- Projects/invoice-system/InvoiceSystem.Application/Services/InvoiceService.cs
+++ Projects/invoice-system/InvoiceSystem.Application/Services/InvoiceService.cs
@@ -1,8 +1,53 @@
-﻿using InvoiceSystem.Application.Services.Interfaces;
+﻿using InvoiceSystem.Application.DTOs.Invoice;
+using InvoiceSystem.Application.DTOs.InvoiceItem;
+using InvoiceSystem.Application.Services.Interfaces;
+using InvoiceSystem.Domain.Common;
+using InvoiceSystem.Domain.Enums;
 
 namespace InvoiceSystem.Application.Services 
 {
     public class InvoiceService : IInvoiceService
     {
+        private readonly IInvoiceRepository _invoiceRepository;
+        private readonly IEmployeeRepository _employeeRepository;
+        public InvoiceService(IEmployeeRepository employeeRepository, IInvoiceRepository invoiceRepository)
+        {
+            _employeeRepository = employeeRepository;
+            _invoiceRepository = invoiceRepository;
+        }
+        public Task<Result<InvoiceDetailsDTO>> CreateInvoiceAsync(InvoiceCreationDTO creationDTO)
+        {
+           var employee = _employeeRepository.GetByIdAsync()
+        }
+
+        public Task<Result<InvoiceItemDTO>> CreateInvoiceItemAsync(Guid invoiceId, InvoiceItemCreationDTO itemDTO)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Result> DeleteInvoiceItemsAsync(Guid invoiceId, Guid itemId, EmployeeType employeeType)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Result<IReadOnlyList<InvoiceSummaryDTO>>> GetAllInvoicesAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Result<InvoiceDetailsDTO>> GetInvoiceByIdAsync(Guid invoiceId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Result> UpdateInvoiceAsync(Guid invoiceId, InvoiceUpdateDTO updateDTO)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Result> VoidInvoiceAsync(Guid invoiceId, EmployeeType employeeType)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
