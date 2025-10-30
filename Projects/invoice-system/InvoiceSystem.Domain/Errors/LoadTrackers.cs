967f879f2b8c2526183151c30df52abfccf45262
zulhamzakaria <m.zulham.zakaria@gmail.com>
2025-10-30 12:04:49 +0800

    - LoadTracker Error code

---
+++ Projects/invoice-system/InvoiceSystem.Domain/Errors/LoadTrackers.cs
@@ -0,0 +1,9 @@
+﻿namespace InvoiceSystem.Domain.Errors;
+
+public static class LoadTrackers
+{
+    public static class Service
+    {
+        public const string ApproverNotFound = "LTR.NO_ELIGIBLE_APPROVER";
+    }
+}
