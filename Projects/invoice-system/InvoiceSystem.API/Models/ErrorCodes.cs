97286f1bbde0de0c45867a673b7a177f886827f5
zulhamzakaria <m.zulham.zakaria@gmail.com>
2025-11-06 08:09:48 +0800

    - generic error codes

---
+++ Projects/invoice-system/InvoiceSystem.API/Models/ErrorCodes.cs
@@ -0,0 +1,31 @@
+﻿using InvoiceSystem.Domain.Common;
+
+namespace InvoiceSystem.API.Models;
+
+public static class ErrorCodes
+{
+    public static Error NotFound<T>() => Build<T>("Not_Found", "not found");
+    public static Error InvalidStatus<T>() => Build<T>("Undefined_Status", "status is invalid");
+
+    private static string GetPrefix<T>()
+    {
+        var name = typeof(T).Name;
+        if (name.StartsWith("I") && name.Length > 1 && char.IsUpper(name[1]))
+        {
+            name = name[1..];
+        }
+        if (name.EndsWith("Service"))
+        {
+            name = name[..^"Service".Length];
+        }
+        return name;
+    }
+    private static Error Build<T>(string key, string messageTemplate)
+    {
+        var prefix = GetPrefix<T>();
+        var message = $"{prefix} {messageTemplate}";
+        var code = $"{prefix}.{key}";
+
+        return Error.Validation(code, message);
+    }
+}
--- Projects/invoice-system/InvoiceSystem.API/obj/Debug/net8.0/InvoiceSystem.API.AssemblyInfo.cs
+++ Projects/invoice-system/InvoiceSystem.API/obj/Debug/net8.0/InvoiceSystem.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("InvoiceSystem.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+84a18e49e7c9bd220e6214e70baf3cee22a82a53")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9fcbadc1546732b10f66def955ab3fd6415b5c55")]
 [assembly: System.Reflection.AssemblyProductAttribute("InvoiceSystem.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("InvoiceSystem.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
