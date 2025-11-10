5e603b63964fce3248bcde1f743ea36c52e18e8c
zulhamzakaria <m.zulham.zakaria@gmail.com>
2025-11-11 07:07:28 +0800

    - optional Email - unused import

--- Projects/invoice-system/InvoiceSystem.Application/DTOs/Employee/EmployeeUpdateDTO.cs
+++ Projects/invoice-system/InvoiceSystem.Application/DTOs/Employee/EmployeeUpdateDTO.cs
@@ -1,10 +1,9 @@
-﻿using InvoiceSystem.Domain.Enums;
-using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace InvoiceSystem.Application.DTOs.Employee;
 
 public record EmployeeUpdateDTO(
     [StringLength(100,MinimumLength =1)] string Name,
-    [Required, EmailAddress, StringLength(100,MinimumLength =1)] string Email,
+    [Required, EmailAddress, StringLength(100,MinimumLength =1)] string? Email,
     [Range(0.0001, (double)decimal.MaxValue)] decimal? ApprovalLimit
     );
