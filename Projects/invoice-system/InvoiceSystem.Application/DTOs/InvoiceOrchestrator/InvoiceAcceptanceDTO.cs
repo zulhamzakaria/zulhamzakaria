a300479c0005bb32494cd8149332af06ab302e40
zulhamzakaria <m.zulham.zakaria@gmail.com>
2025-11-10 13:11:37 +0800

    - dto for invoice acceptance

---
+++ Projects/invoice-system/InvoiceSystem.Application/DTOs/InvoiceOrchestrator/InvoiceAcceptanceDTO.cs
@@ -0,0 +1,9 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace InvoiceSystem.Application.DTOs.InvoiceOrchestrator;
+
+public record InvoiceAcceptanceDTO(
+    [Required]
+    Guid approverId,
+    [StringLength(maximumLength:500, MinimumLength =1)]
+    string? Remarks);
--- Projects/invoice-system/InvoiceSystem.Application/obj/Debug/net8.0/InvoiceSystem.Application.AssemblyInfo.cs
+++ Projects/invoice-system/InvoiceSystem.Application/obj/Debug/net8.0/InvoiceSystem.Application.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("InvoiceSystem.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9803d8b5622dcd732c1b7691a38895dfeed397f8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e8b79f769f7bf23cecf7230fa6be8c66cf392a2d")]
 [assembly: System.Reflection.AssemblyProductAttribute("InvoiceSystem.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("InvoiceSystem.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
