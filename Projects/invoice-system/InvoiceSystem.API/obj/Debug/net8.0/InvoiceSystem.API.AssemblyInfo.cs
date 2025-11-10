d01f06c6b5d19426fdc7c9b2806cbceceada5738
zulhamzakaria <m.zulham.zakaria@gmail.com>
2025-11-11 07:14:37 +0800

    - ran

--- Projects/invoice-system/InvoiceSystem.API/obj/Debug/net8.0/InvoiceSystem.API.AssemblyInfo.cs
+++ Projects/invoice-system/InvoiceSystem.API/obj/Debug/net8.0/InvoiceSystem.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("InvoiceSystem.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5e603b63964fce3248bcde1f743ea36c52e18e8c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bd4bf96fea3df957cb5953cb8b34d8ca078138c4")]
 [assembly: System.Reflection.AssemblyProductAttribute("InvoiceSystem.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("InvoiceSystem.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
