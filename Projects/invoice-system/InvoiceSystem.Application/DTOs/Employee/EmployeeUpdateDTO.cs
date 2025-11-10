721c6270f355b4f3d97c9890551f6ec6296e91e6
zulhamzakaria <m.zulham.zakaria@gmail.com>
2025-11-11 07:06:40 +0800

    - record props in-line with attr

--- Projects/invoice-system/InvoiceSystem.Application/DTOs/Employee/EmployeeUpdateDTO.cs
+++ Projects/invoice-system/InvoiceSystem.Application/DTOs/Employee/EmployeeUpdateDTO.cs
@@ -4,10 +4,7 @@ using System.ComponentModel.DataAnnotations;
 namespace InvoiceSystem.Application.DTOs.Employee;
 
 public record EmployeeUpdateDTO(
-    [StringLength(100,MinimumLength =1)]
-    string Name,
-    [Required, EmailAddress, StringLength(100,MinimumLength =1)]
-    string Email,
-    [property: Range(0.0001, (double)decimal.MaxValue)]
-    decimal? ApprovalLimit
+    [StringLength(100,MinimumLength =1)] string Name,
+    [Required, EmailAddress, StringLength(100,MinimumLength =1)] string Email,
+    [Range(0.0001, (double)decimal.MaxValue)] decimal? ApprovalLimit
     );
