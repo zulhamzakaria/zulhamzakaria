5169a7e2de8254869df0772da3ffff5f6813ef3e
zulhamzakaria <m.zulham.zakaria@gmail.com>
2025-11-11 07:03:39 +0800

    - tagged Email as required

--- Projects/invoice-system/InvoiceSystem.Application/DTOs/Employee/EmployeeUpdateDTO.cs
+++ Projects/invoice-system/InvoiceSystem.Application/DTOs/Employee/EmployeeUpdateDTO.cs
@@ -4,9 +4,9 @@ using System.ComponentModel.DataAnnotations;
 namespace InvoiceSystem.Application.DTOs.Employee;
 
 public record EmployeeUpdateDTO(
-    [property: StringLength(100,MinimumLength =1)]
+    [StringLength(100,MinimumLength =1)]
     string Name,
-    [property: EmailAddress, StringLength(100,MinimumLength =1)]
+    [Required, EmailAddress, StringLength(100,MinimumLength =1)]
     string Email,
     [property: Range(0.0001, (double)decimal.MaxValue)]
     decimal? ApprovalLimit
