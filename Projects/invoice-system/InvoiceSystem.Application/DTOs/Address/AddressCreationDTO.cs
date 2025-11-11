bb5655ba35a75f40bbb1ffafddb338ec564860f5
zulhamzakaria <m.zulham.zakaria@gmail.com>
2025-11-11 17:57:37 +0800

    - removed AddressType prop caus it's beind set explitcitly in the mapping service

--- Projects/invoice-system/InvoiceSystem.Application/DTOs/Address/AddressCreationDTO.cs
+++ Projects/invoice-system/InvoiceSystem.Application/DTOs/Address/AddressCreationDTO.cs
@@ -1,13 +1,12 @@
-﻿using InvoiceSystem.Domain.Enums;
-using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace InvoiceSystem.Application.DTOs.Address;
 
 public record AddressCreationDTO(
     [Required, StringLength(200, MinimumLength = 1)] string Street,
     [Required, StringLength(10, MinimumLength =1)] string Zipcode,
-    [ Required, StringLength(100, MinimumLength =1)] string City,
-    [ Required, StringLength(100,MinimumLength =1)] string State,
-    [ Required, StringLength(50, MinimumLength =1)] string Country,
-    [ Required] AddressType AddressType
+    [Required, StringLength(100, MinimumLength =1)] string City,
+    [Required, StringLength(100,MinimumLength =1)] string State,
+    [Required, StringLength(50, MinimumLength =1)] string Country
+    //[Required] AddressType AddressType
     );
