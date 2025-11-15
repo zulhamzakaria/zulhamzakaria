152b39e8209f36aca38681787e0c03d2f6efcbb3
zulhamzakaria <m.zulham.zakaria@gmail.com>
2025-11-15 14:08:20 +0800

    - migration

--- Projects/invoice-system/InvoiceSystem.Infrastructure/obj/Debug/net8.0/InvoiceSystem.Infrastructure.AssemblyInfo.cs
+++ Projects/invoice-system/InvoiceSystem.Infrastructure/obj/Debug/net8.0/InvoiceSystem.Infrastructure.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("InvoiceSystem.Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7849401f5a6189706e006fdeaac15060f903b2d7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ab14cd3a9833f1b2e7db90ae286d6347e9d13a57")]
 [assembly: System.Reflection.AssemblyProductAttribute("InvoiceSystem.Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("InvoiceSystem.Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
