ab14cd3a9833f1b2e7db90ae286d6347e9d13a57
zulhamzakaria <m.zulham.zakaria@gmail.com>
2025-11-15 13:27:22 +0800

    - migraions

---
+++ Projects/invoice-system/InvoiceSystem.Infrastructure/Migrations/20251115052648_updated-invoice-invoiceitem-relation.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace InvoiceSystem.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class updatedinvoiceinvoiceitemrelation : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_InvoiceItems_Invoices_InvoiceId1",
+                table: "InvoiceItems");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "InvoiceId1",
+                table: "InvoiceItems",
+                type: "uuid",
+                nullable: true,
+                oldClrType: typeof(Guid),
+                oldType: "uuid");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_InvoiceItems_Invoices_InvoiceId1",
+                table: "InvoiceItems",
+                column: "InvoiceId1",
+                principalTable: "Invoices",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_InvoiceItems_Invoices_InvoiceId1",
+                table: "InvoiceItems");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "InvoiceId1",
+                table: "InvoiceItems",
+                type: "uuid",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"),
+                oldClrType: typeof(Guid),
+                oldType: "uuid",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_InvoiceItems_Invoices_InvoiceId1",
+                table: "InvoiceItems",
+                column: "InvoiceId1",
+                principalTable: "Invoices",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
