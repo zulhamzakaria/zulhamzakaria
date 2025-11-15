d9931354a4072fe816fb6846d5c44bf7f01234e9
zulhamzakaria <m.zulham.zakaria@gmail.com>
2025-11-15 14:42:36 +0800

    - migrations

--- Projects/invoice-system/InvoiceSystem.Application/obj/Debug/net8.0/InvoiceSystem.Application.AssemblyInfo.cs
+++ Projects/invoice-system/InvoiceSystem.Application/obj/Debug/net8.0/InvoiceSystem.Application.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("InvoiceSystem.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b740c4075f17953a47acf11d86fa98fd632248e8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fab7fb60fe525990172797ba2f2b83a72fbb1b29")]
 [assembly: System.Reflection.AssemblyProductAttribute("InvoiceSystem.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("InvoiceSystem.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Projects/invoice-system/InvoiceSystem.Infrastructure/obj/Debug/net8.0/InvoiceSystem.Infrastructure.AssemblyInfo.cs
+++ Projects/invoice-system/InvoiceSystem.Infrastructure/obj/Debug/net8.0/InvoiceSystem.Infrastructure.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("InvoiceSystem.Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+05b75443a434ef0bfec9831940ffeb8927031d27")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fab7fb60fe525990172797ba2f2b83a72fbb1b29")]
 [assembly: System.Reflection.AssemblyProductAttribute("InvoiceSystem.Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("InvoiceSystem.Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
