bfa14b46c034fe3d6b09fad72281646d7722268e
zulhamzakaria <m.zulham.zakaria@gmail.com>
2025-11-16 09:19:41 +0800

    - somn

--- Projects/invoice-system/InvoiceSystem.Infrastructure/obj/Debug/net8.0/InvoiceSystem.Infrastructure.AssemblyInfo.cs
+++ Projects/invoice-system/InvoiceSystem.Infrastructure/obj/Debug/net8.0/InvoiceSystem.Infrastructure.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("InvoiceSystem.Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+905f5389674447d1329c4ed9ab8745966f92e94c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5f1459e1b00e559315476f78b333875806bf6182")]
 [assembly: System.Reflection.AssemblyProductAttribute("InvoiceSystem.Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("InvoiceSystem.Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
