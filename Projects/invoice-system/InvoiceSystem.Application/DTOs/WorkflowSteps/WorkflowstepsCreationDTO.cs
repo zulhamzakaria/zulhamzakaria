fe977539e70963ebcb7f64293fda45bb6137472f
zulhamzakaria <m.zulham.zakaria@gmail.com>
2025-11-16 08:34:09 +0800

    - ran a build - added a comment for optional fields

--- Projects/invoice-system/InvoiceSystem.Application/DTOs/WorkflowSteps/WorkflowstepsCreationDTO.cs
+++ Projects/invoice-system/InvoiceSystem.Application/DTOs/WorkflowSteps/WorkflowstepsCreationDTO.cs
@@ -4,6 +4,7 @@ using System.ComponentModel.DataAnnotations;
 namespace InvoiceSystem.Application.DTOs.WorkflowSteps;
 
 public record WorkflowstepsCreationDTO(
+    ///<summary>ApproverId and Reason are not required by Submission</summary>
     [Required] WorkflowStepType WorkflowStepType,
     Guid? ApproverId,
     [StringLength(500, MinimumLength = 1)] string? Reason
