1ba2e174799f7ec6cdb5eaa865fd3c3ecd505a45
zulhamzakaria <m.zulham.zakaria@gmail.com>
2025-11-16 14:23:59 +0800

    - packages for Swagger documentation and annotation

--- Projects/invoice-system/InvoiceSystem.API/obj/Debug/net8.0/InvoiceSystem.API.AssemblyInfo.cs
+++ Projects/invoice-system/InvoiceSystem.API/obj/Debug/net8.0/InvoiceSystem.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("InvoiceSystem.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ade1b6918978b7f37f5363a395104101fab43782")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2526f6f7afa3229847cd797d2a16305bebbe8dbe")]
 [assembly: System.Reflection.AssemblyProductAttribute("InvoiceSystem.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("InvoiceSystem.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
