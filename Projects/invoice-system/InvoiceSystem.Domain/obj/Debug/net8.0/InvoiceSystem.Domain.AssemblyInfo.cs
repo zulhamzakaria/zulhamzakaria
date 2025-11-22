cd1f8c81aba9871ee77fb4455300fa393ae4a3e8
zulhamzakaria <m.zulham.zakaria@gmail.com>
2025-11-22 09:02:30 +0800

    - ran a build - added PendingManagerApproval to the service

--- Projects/invoice-system/InvoiceSystem.Domain/obj/Debug/net8.0/InvoiceSystem.Domain.AssemblyInfo.cs
+++ Projects/invoice-system/InvoiceSystem.Domain/obj/Debug/net8.0/InvoiceSystem.Domain.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("InvoiceSystem.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7e37cf9b443d54f410ee9b30c8eb534b46bc38f2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3ef24bb975261ad84cfc85d045c1631e94a0b054")]
 [assembly: System.Reflection.AssemblyProductAttribute("InvoiceSystem.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("InvoiceSystem.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
