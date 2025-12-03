2c204114f050f63092b3331c3d721d904908c70a
zulhamzakaria <m.zulham.zakaria@gmail.com>
2025-12-03 19:41:59 +0800

    - new entry for the Invoice Repo for returning queryable Invoices

--- Projects/invoice-system/InvoiceSystem.Domain/Repositories/IInvoiceRepository.cs
+++ Projects/invoice-system/InvoiceSystem.Domain/Repositories/IInvoiceRepository.cs
@@ -5,6 +5,7 @@ public interface IInvoiceRepository
     Task<Invoice?> GetByIdAsync(Guid id);
     Task<IReadOnlyList<Invoice>> GetAllAsync();
     Task<bool> ExistsByInvoiceNoAsync(string invoiceNo);
+    IQueryable<Invoice> QueryAll();
     Task AddAsync(Invoice invoice);
     Task AddItemAsync(InvoiceItem item);
     Task UpdateAsync(Invoice invoice);
