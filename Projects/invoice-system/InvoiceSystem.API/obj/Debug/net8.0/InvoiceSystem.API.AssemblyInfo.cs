f3f22e3ee19e9aedbd48c19dc549fbcaad88e641
zulhamzakaria <m.zulham.zakaria@gmail.com>
2025-12-07 06:58:41 +0800

    - ran a build

--- Projects/invoice-system/InvoiceSystem.API/obj/Debug/net8.0/InvoiceSystem.API.AssemblyInfo.cs
+++ Projects/invoice-system/InvoiceSystem.API/obj/Debug/net8.0/InvoiceSystem.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("InvoiceSystem.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+891fdd89f0901559fe9dda922ca81a3bc29e10b1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c2c270c038b7155b88443e048b1adbf202435bfe")]
 [assembly: System.Reflection.AssemblyProductAttribute("InvoiceSystem.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("InvoiceSystem.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Projects/invoice-system/InvoiceSystem.Domain/obj/Debug/net8.0/InvoiceSystem.Domain.AssemblyInfo.cs
+++ Projects/invoice-system/InvoiceSystem.Domain/obj/Debug/net8.0/InvoiceSystem.Domain.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("InvoiceSystem.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0ad1e9a68690598782a98f5c9fe9346a5eaf7716")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c2c270c038b7155b88443e048b1adbf202435bfe")]
 [assembly: System.Reflection.AssemblyProductAttribute("InvoiceSystem.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("InvoiceSystem.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
