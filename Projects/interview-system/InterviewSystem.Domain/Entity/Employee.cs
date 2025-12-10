ab7f53370739fa456993bbb570c17f401e7e2d75
zulhamzakaria <m.zulham.zakaria@gmail.com>
2025-12-11 07:03:01 +0800

    - basic Employee entity

---
+++ Projects/interview-system/InterviewSystem.Domain/Entity/Employee.cs
@@ -0,0 +1,23 @@
+﻿using InterviewSystem.Domain.Common.Enum;
+
+namespace InterviewSystem.Domain.Entity;
+
+public class Employee
+{
+    public Guid Id { get; private set; }
+    public string Name { get; private set; }
+    public EmployeeType EmployeeType { get; private set; }
+    public EmployeeDepartment EmployeeDepartment { get; private set; }
+    public EmployeePosition EmployeePosition { get; private set; }
+
+    public Employee(){} //required by EF Core
+
+    public Employee(Guid id, string name, EmployeeType type, EmployeeDepartment department, EmployeePosition position)
+    {
+        Id = id;
+        Name = name;
+        EmployeeType = type;
+        EmployeeDepartment = department;
+        EmployeePosition = position;
+    }
+}
