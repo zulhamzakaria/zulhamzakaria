52b47af7179f7caf43043e7d36ac7ecfa20b612e
zulhamzakaria <m.zulham.zakaria@gmail.com>
2025-12-11 19:38:33 +0800

    - basic Candidate entity

---
+++ Projects/interview-system/InterviewSystem.Domain/Common/Enum/AppliedPosition.cs
@@ -0,0 +1,12 @@
+﻿namespace InterviewSystem.Domain.Common.Enum;
+
+public enum AppliedPosition
+{
+    Clerk,
+    TeaLady,
+    JuniorEngineer,
+    SeniorEngineer,
+    TechnicalLead,
+    AssistantManager,
+    Manager
+}
---
+++ Projects/interview-system/InterviewSystem.Domain/Entity/Candidate.cs
@@ -0,0 +1,26 @@
+﻿using InterviewSystem.Domain.Common.Enum;
+
+namespace InterviewSystem.Domain.Entity;
+
+public class Candidate: EntityBase
+{
+    public Guid Id { get; private set; }
+    public string Name { get; private set; } = string.Empty;
+    public string Email { get; private set; } = string.Empty;
+    public string PhoneNumber { get; private set; } = string.Empty;
+    public AppliedPosition AppliedPosition { get; private set; }
+
+    private Candidate()
+    {
+        //EF Core needs this
+    }
+
+    private Candidate(Guid id, string name, string email, string phoneNumber, AppliedPosition appliedPosition)
+    {
+        Id = id;
+        Name = name;
+        Email = email;
+        PhoneNumber = phoneNumber;
+        AppliedPosition = appliedPosition;
+    }
+}
