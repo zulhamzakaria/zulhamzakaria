699cd1f3cf3e0135035b261c3b943d61eb9c8117
zulhamzakaria <m.zulham.zakaria@gmail.com>
2025-12-12 06:36:34 +0800

    - Result<T> for error handling

---
+++ Projects/interview-system/InterviewSystem.Domain/Common/ErrorHandling/GenericResult.cs
@@ -0,0 +1,30 @@
+﻿namespace InterviewSystem.Domain.Common.ErrorHandling;
+
+public class Result<T>
+{
+    public bool IsSuccess { get;}
+    public bool IsFailure => !IsSuccess;
+
+    public T? Value { get; }
+    public string? ErrorMessage { get; }
+    public string? ErrorCode { get; }
+
+    private Result(bool isSuccess, T? value, string errorMessage, string? errorCode)
+    {
+        IsSuccess = isSuccess;
+        Value = value;
+        ErrorMessage = errorMessage;
+        ErrorCode = errorCode;
+    }
+
+    public static Result<T> Success(T value)
+        => new Result<T>(true, value, string.Empty, null);
+
+    public static Result<T> Failure(string errorMessage, string errorCode)
+    {
+        if (string.IsNullOrWhiteSpace(errorCode))
+            throw new ArgumentNullException("Error code must be provided!");
+
+        return new Result<T>(false, default, errorMessage, errorCode);
+    }
+}
