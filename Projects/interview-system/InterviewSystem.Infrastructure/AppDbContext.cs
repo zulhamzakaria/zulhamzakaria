e7137de50298b041b45b98806968c283d386e4b2
zulhamzakaria <m.zulham.zakaria@gmail.com>
2025-12-13 14:11:53 +0800

    - pieced-up appdbcontext

--- Projects/interview-system/InterviewSystem.Infrastructure/AppDbContext.cs
+++ Projects/interview-system/InterviewSystem.Infrastructure/AppDbContext.cs
@@ -6,7 +6,71 @@ namespace InterviewSystem.Infrastructure;
 public class AppDbContext : DbContext
 {
     public AppDbContext(DbContextOptions options) : base(options)
-    {}
+    { }
 
     public DbSet<Employee> Employees => Set<Employee>();
+    public DbSet<Candidate> Candidate => Set<Candidate>();
+    public DbSet<InterviewRound> InterviewRounds => Set<InterviewRound>();
+    public DbSet<InterviewTask> InterviewTasks => Set<InterviewTask>();
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        ConfigureInterviewRound(modelBuilder);
+        ConfigureInterviewTask(modelBuilder);
+
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(AppDbContext).Assembly);
+        base.OnModelCreating(modelBuilder);
+    }
+
+    protected override void ConfigureConventions(ModelConfigurationBuilder configurationBuilder)
+    {
+        configurationBuilder.Properties<Enum>()
+            .HaveConversion<string>();
+        base.ConfigureConventions(configurationBuilder);
+    }
+
+    private void ConfigureInterviewTask(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<InterviewTask>(builder =>
+        {
+            builder.HasKey(x => x.Id);
+            builder.OwnsOne(x => x.Evaluation, eval =>
+            {
+                eval.Property(ev => ev.Passed).IsRequired();
+                eval.Property(ev => ev.Note);
+            });
+        });
+
+        modelBuilder.Entity<InterviewTask>()
+            .HasOne<Candidate>()
+            .WithMany()
+            .HasForeignKey(x => x.CandidateId)
+            .OnDelete(DeleteBehavior.Restrict);
+
+        modelBuilder.Entity<InterviewTask>()
+            .HasOne<InterviewRound>()
+            .WithMany()
+            .HasForeignKey(x => x.InterviewRoundId)
+            .OnDelete(DeleteBehavior.Restrict);
+    }
+
+    private void ConfigureInterviewRound(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<InterviewRound>(builder =>
+        {
+            builder.HasKey(x => x.Id);
+            builder.OwnsMany(x => x.Items, item =>
+            {
+                item.WithOwner().HasForeignKey("InterviewRoundId");
+                item.Property<int>("Id");
+                item.HasKey("Id");
+
+                item.Property(i => i.Sequence).IsRequired();
+                item.Property(i => i.Position).IsRequired();
+
+                item.ToTable("InterviewRoundItems");
+
+            });
+        });
+    }
 }
