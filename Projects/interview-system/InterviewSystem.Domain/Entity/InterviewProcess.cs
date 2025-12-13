21a3c073b5751a90b89af669f6c1357c5afe709a
zulhamzakaria <m.zulham.zakaria@gmail.com>
2025-12-13 15:45:26 +0800

    - Entity for overseeing the process - WIP

---
+++ Projects/interview-system/InterviewSystem.Domain/Entity/InterviewProcess.cs
@@ -0,0 +1,13 @@
+﻿using InterviewSystem.Domain.Common.Enum;
+
+namespace InterviewSystem.Domain.Entity;
+
+public class InterviewProcess : EntityBase
+{
+    public Guid Id { get; private set; }
+    public Guid CandidateId { get; private set; }
+    public EmployeeDepartment Department { get; private set; }
+    public int CurrentSequence { get; private set; }
+    public interview
+
+}
