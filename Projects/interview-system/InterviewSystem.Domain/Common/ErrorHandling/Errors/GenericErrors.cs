68253adf947b4a986210e97e9b998f2fd075f10a
zulhamzakaria <m.zulham.zakaria@gmail.com>
2025-12-15 10:10:16 +0800

    - Error construct for InvalidEnumValue

--- Projects/interview-system/InterviewSystem.Domain/Common/ErrorHandling/Errors/GenericErrors.cs
+++ Projects/interview-system/InterviewSystem.Domain/Common/ErrorHandling/Errors/GenericErrors.cs
@@ -4,6 +4,8 @@ public static class GenericErrors
 {
     public static Error Required(string fieldName)
         => new($"{fieldName.ToUpper()}_REQUIRED", $"{fieldName} is required");
+    public static Error InvalidEnumValue()
+        => new($"UNDEFINED_ENUM_VALUE", $"The enum value provided is invalid");
     public static Error InvalidLength(string fieldName, int min, int max)
         => new($"{fieldName.ToUpper()}_LENGTH_INVALID", $"{fieldName} length must be between {min} and {max} characters");
 }
