47694bb860c44f8bddf7f15b188404bc7940acfa
zulhamzakaria <m.zulham.zakaria@gmail.com>
2025-12-15 10:14:21 +0800

    - rename the folder for clashing namespace - WIP

--- Projects/interview-system/InterviewSystem.Domain/Common/ErrorHandling/Errors/GenericErrors.cs
+++ Projects/interview-system/InterviewSystem.Domain/Common/ErrorHandling/Errors/GenericErrors.cs
@@ -4,8 +4,9 @@ public static class GenericErrors
 {
     public static Error Required(string fieldName)
         => new($"{fieldName.ToUpper()}_REQUIRED", $"{fieldName} is required");
-    public static Error InvalidEnumValue()
+    public static Error InvalidEnumValue<TEnum>(TEnum value) where TEnum : struct, Enum
         => new($"UNDEFINED_ENUM_VALUE", $"The enum value provided is invalid");
+
     public static Error InvalidLength(string fieldName, int min, int max)
         => new($"{fieldName.ToUpper()}_LENGTH_INVALID", $"{fieldName} length must be between {min} and {max} characters");
 }
