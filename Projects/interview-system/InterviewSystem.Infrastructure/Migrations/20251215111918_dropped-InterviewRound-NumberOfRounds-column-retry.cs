9622b4a218b8a331bf4675f5b52cd53a07a6eacc
zulhamzakaria <m.zulham.zakaria@gmail.com>
2025-12-15 19:24:48 +0800

    - migrations

---
+++ Projects/interview-system/InterviewSystem.Infrastructure/Migrations/20251215111918_dropped-InterviewRound-NumberOfRounds-column-retry.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace InterviewSystem.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class droppedInterviewRoundNumberOfRoundscolumnretry : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
