825f701d171ea7481150970fd7ffd5ef9f4a85f7
zulhamzakaria <m.zulham.zakaria@gmail.com>
2025-12-15 10:19:42 +0800

    - generic Error for Enums

--- Projects/interview-system/InterviewSystem.Domain/Common/ErrorHandling/Errors/GenericErrors.cs
+++ Projects/interview-system/InterviewSystem.Domain/Common/ErrorHandling/Errors/GenericErrors.cs
@@ -5,7 +5,7 @@ public static class GenericErrors
     public static Error Required(string fieldName)
         => new($"{fieldName.ToUpper()}_REQUIRED", $"{fieldName} is required");
     public static Error InvalidEnumValue<TEnum>(TEnum value) where TEnum : struct, Enum
-        => new($"UNDEFINED_ENUM_VALUE", $"The enum value provided is invalid");
+        => new($"UNDEFINED_{typeof(TEnum).Name.ToUpper()}_VALUE", $"The value provided for {typeof(TEnum).Name} is invalid");
 
     public static Error InvalidLength(string fieldName, int min, int max)
         => new($"{fieldName.ToUpper()}_LENGTH_INVALID", $"{fieldName} length must be between {min} and {max} characters");
