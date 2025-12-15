441aaeff9abcb7077e8c91b44982026fe4f158f5
zulhamzakaria <m.zulham.zakaria@gmail.com>
2025-12-15 16:09:58 +0800

    - private function for splitting/converting the fieldName into Field Name

--- Projects/interview-system/InterviewSystem.Domain/Common/ErrorHandling/Errors/GenericErrors.cs
+++ Projects/interview-system/InterviewSystem.Domain/Common/ErrorHandling/Errors/GenericErrors.cs
@@ -3,11 +3,28 @@
 public static class GenericErrors
 {
     public static Error Required(string fieldName)
-        => new($"{fieldName.ToUpper()}_REQUIRED", $"{fieldName} is required");
+        => new($"{fieldName.ToUpper()}_REQUIRED", $"{Humanize(fieldName)} is required");
     public static Error InvalidEnumValue<TEnum>(TEnum value) where TEnum : struct, Enum
         => new($"UNDEFINED_{typeof(TEnum).Name.ToUpper()}_VALUE", $"The value provided for {typeof(TEnum).Name} is invalid");
     public static Error InvalidLength(string fieldName, int min, int max)
-        => new($"{fieldName.ToUpper()}_LENGTH_INVALID", $"{fieldName} length must be between {min} and {max} characters");
+        => new($"{fieldName.ToUpper()}_LENGTH_INVALID", $"{Humanize(fieldName)} length must be between {min} and {max} characters");
     public static Error InvalidIntValue(string fieldName)
-        => new($"{fieldName.ToUpper()}_VALUE_INVALID", $"{fieldName} must be bigger than 0");
+        => new($"{fieldName.ToUpper()}_VALUE_INVALID", $"{Humanize(fieldName)} must be bigger than 0");
+
+    private static string Humanize(string input)
+    {
+        if (string.IsNullOrWhiteSpace(input)) return string.Empty;
+
+        // split camelCase / PascalCase into words
+        // get the first capital case in the middle of the fieldname i.e I in candidateId 
+        var result = System.Text.RegularExpressions.Regex.Replace(
+            input,
+            "(\\B[A-Z])",
+            " $1"
+        );
+
+        // capitalize first letter
+        return char.ToUpper(result[0]) + result.Substring(1);
+    }
+
 }
