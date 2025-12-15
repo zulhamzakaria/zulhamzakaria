db67f30306f667ed6426f5dd5287adfd55214936
zulhamzakaria <m.zulham.zakaria@gmail.com>
2025-12-15 18:32:06 +0800

    - interviewrounditem Create()

--- Projects/interview-system/InterviewSystem.Domain/Entity/InterviewRoundItem.cs
+++ Projects/interview-system/InterviewSystem.Domain/Entity/InterviewRoundItem.cs
@@ -1,4 +1,6 @@
 ﻿using InterviewSystem.Domain.Common.Enums;
+using InterviewSystem.Domain.Common.ErrorHandling;
+using InterviewSystem.Domain.Common.ErrorHandling.Errors;
 
 namespace InterviewSystem.Domain.Entity;
 
@@ -12,9 +14,36 @@ public class InterviewRoundItem
         //EF Core needs this
     }
 
-    private InterviewRoundItem(int sequence, EmployeePosition employee)
+    //private InterviewRoundItem(int sequence, EmployeePosition employee)
+    //{
+    //    Sequence = sequence;
+    //    Position = employee;
+    //}
+
+   public static Result<InterviewRoundItem> Create(int sequence, EmployeePosition position)
     {
-        Sequence = sequence;
-        Position = employee;
+        List<Error> errors = new();
+
+        if(sequence <= 0)
+        {
+            errors.Add(GenericErrors.InvalidIntValue(nameof(sequence)));
+        }
+        if(Enum.IsDefined(position) is false)
+        {
+            errors.Add(GenericErrors.InvalidEnumValue(position));
+        }
+
+        if (errors.Any())
+        {
+            return Result<InterviewRoundItem>.Failure(errors);
+        }
+
+        InterviewRoundItem item = new()
+        {
+            Sequence = sequence,
+            Position = position
+        };
+
+        return Result<InterviewRoundItem>.Success(item);
     }
 }
