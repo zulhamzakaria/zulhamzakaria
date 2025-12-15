46b13743a8af3973a1aa126923e10ecba98f03bb
zulhamzakaria <m.zulham.zakaria@gmail.com>
2025-12-15 11:09:43 +0800

    - spacings

--- Projects/interview-system/InterviewSystem.Domain/Common/ErrorHandling/Errors/GenericErrors.cs
+++ Projects/interview-system/InterviewSystem.Domain/Common/ErrorHandling/Errors/GenericErrors.cs
@@ -6,7 +6,6 @@ public static class GenericErrors
         => new($"{fieldName.ToUpper()}_REQUIRED", $"{fieldName} is required");
     public static Error InvalidEnumValue<TEnum>(TEnum value) where TEnum : struct, Enum
         => new($"UNDEFINED_{typeof(TEnum).Name.ToUpper()}_VALUE", $"The value provided for {typeof(TEnum).Name} is invalid");
-
     public static Error InvalidLength(string fieldName, int min, int max)
         => new($"{fieldName.ToUpper()}_LENGTH_INVALID", $"{fieldName} length must be between {min} and {max} characters");
 }
