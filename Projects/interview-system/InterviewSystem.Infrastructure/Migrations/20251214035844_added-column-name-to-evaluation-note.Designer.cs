105e68b28129c7a7736130aad8e4b5152111a9e4
zulhamzakaria <m.zulham.zakaria@gmail.com>
2025-12-15 09:30:47 +0800

    - added column name for evaluation note

--- Projects/interview-system/InterviewSystem.Infrastructure/Migrations/20251214035844_added-column-name-to-evaluation-note.Designer.cs
+++ Projects/interview-system/InterviewSystem.Infrastructure/Migrations/20251214035844_added-column-name-to-evaluation-note.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using InterviewSystem.Infrastructure;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace InterviewSystem.Infrastructure.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20251214035844_added-column-name-to-evaluation-note")]
+    partial class addedcolumnnametoevaluationnote
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Projects/interview-system/InterviewSystem.Infrastructure/Migrations/20251214035844_added-column-name-to-evaluation-note.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace InterviewSystem.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class addedcolumnnametoevaluationnote : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Evaluation_Note",
+                table: "InterviewTasks",
+                newName: "EvaluationNote");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "EvaluationNote",
+                table: "InterviewTasks",
+                newName: "Evaluation_Note");
+        }
+    }
+}
