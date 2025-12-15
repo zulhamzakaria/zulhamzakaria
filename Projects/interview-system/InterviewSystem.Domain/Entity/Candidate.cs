bdd903df8d047c2d984144a017f313943fe7bd2b
zulhamzakaria <m.zulham.zakaria@gmail.com>
2025-12-15 10:53:48 +0800

    - min/maxlength for fields

--- Projects/interview-system/InterviewSystem.Domain/Entity/Candidate.cs
+++ Projects/interview-system/InterviewSystem.Domain/Entity/Candidate.cs
@@ -6,6 +6,11 @@ namespace InterviewSystem.Domain.Entity;
 
 public class Candidate : EntityBase
 {
+    const int MinLength = 1;
+    const int MaxNameLength = 100;
+    const int MaxEmailLength = 100;
+    const int MaxPhoneNumberLength = 20;
+
     public Guid Id { get; private set; }
     public string Name { get; private set; } = string.Empty;
     public string Email { get; private set; } = string.Empty;
@@ -17,14 +22,14 @@ public class Candidate : EntityBase
         //EF Core needs this
     }
 
-    private Candidate(Guid id, string name, string email, string phoneNumber, AppliedPosition appliedPosition)
-    {
-        Id = id;
-        Name = name;
-        Email = email;
-        PhoneNumber = phoneNumber;
-        AppliedPosition = appliedPosition;
-    }
+    //private Candidate(Guid id, string name, string email, string phoneNumber, AppliedPosition appliedPosition)
+    //{
+    //    Id = id;
+    //    Name = name;
+    //    Email = email;
+    //    PhoneNumber = phoneNumber;
+    //    AppliedPosition = appliedPosition;
+    //}
 
     public static Result<Candidate> Create(string name, string email, string phoneNumber, AppliedPosition appliedPosition)
     {
@@ -37,19 +42,28 @@ public class Candidate : EntityBase
         if (string.IsNullOrWhiteSpace(phoneNumber))
             return Result<Candidate>.Failure(GenericErrors.Required(nameof(phoneNumber)));
 
+        if(name.Length > MaxNameLength)
+            return Result<Candidate>.Failure(GenericErrors.InvalidLength(nameof(name), MinLength, MaxNameLength));
+
+        if(email.Length > MaxEmailLength)
+            return Result<Candidate>.Failure(GenericErrors.InvalidLength(nameof(name), MinLength, MaxEmailLength));
+
+        if(phoneNumber.Length > MaxPhoneNumberLength)
+            return Result<Candidate>.Failure(GenericErrors.InvalidLength(nameof(name), MinLength, MaxPhoneNumberLength));
+
         if (Enum.IsDefined(typeof(AppliedPosition), appliedPosition) is false)
             return Result<Candidate>.Failure(GenericErrors.InvalidEnumValue(appliedPosition));
 
-        var candidate = new Candidate(
-            Guid.NewGuid(),
-            name,
-            email,
-            phoneNumber,
-            appliedPosition
-            );
+        var candidate = new Candidate()
+        {
+            Id = Guid.NewGuid(),
+            Name = name,
+            Email = email,
+            PhoneNumber = phoneNumber,
+            AppliedPosition = appliedPosition
+        };
 
         return Result<Candidate>.Success(candidate);
-
     }
 
     public void UpdateEmail(string email)
