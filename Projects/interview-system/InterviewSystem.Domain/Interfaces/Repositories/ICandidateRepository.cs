0a51026216a042260994cc1003653030e69887c1
zulhamzakaria <m.zulham.zakaria@gmail.com>
2025-12-16 14:33:09 +0800

    - added comment/note

--- Projects/interview-system/InterviewSystem.Domain/Interfaces/Repositories/ICandidateRepository.cs
+++ Projects/interview-system/InterviewSystem.Domain/Interfaces/Repositories/ICandidateRepository.cs
@@ -4,7 +4,7 @@ namespace InterviewSystem.Domain.Interfaces.Repositories;
 
 public interface ICandidateRepository
 {
-    Task<IReadOnlyCollection<Candidate>> GetAllAsync();
+    Task<IReadOnlyCollection<Candidate>> GetAllAsync(); //ToList always return an empty list
     Task<Candidate?> GetByIdAsync(Guid id); //maybe null hence the nullable
     Task AddAsync(Candidate candidate);
 }
