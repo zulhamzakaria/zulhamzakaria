d10a7e32af083ff38f91dac40f1eccea87eb36f6
zulhamzakaria <m.zulham.zakaria@gmail.com>
2025-12-16 10:44:19 +0800

    - Candidate interface Repo

---
+++ Projects/interview-system/InterviewSystem.Domain/Interfaces/Repositories/ICandidateRepository.cs
@@ -0,0 +1,10 @@
+﻿using InterviewSystem.Domain.Entity;
+
+namespace InterviewSystem.Domain.Interfaces.Repositories;
+
+public interface ICandidateRepository
+{
+    Task<IReadOnlyCollection<Candidate>> GetAllAsync();
+    Task<Candidate?> GetByIdAsync(Guid id); //maybe null hence the nullable
+    Task AddAsync(Candidate candidate);
+}
