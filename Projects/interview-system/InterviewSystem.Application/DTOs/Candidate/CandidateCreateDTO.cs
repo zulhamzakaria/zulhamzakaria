4ba9625f5cae8eda593927ab40f657687087bd35
zulhamzakaria <m.zulham.zakaria@gmail.com>
2025-12-17 13:33:02 +0800

    - Candidate DTOs

---
+++ Projects/interview-system/InterviewSystem.Application/DTOs/Candidate/CandidateCreateDTO.cs
@@ -0,0 +1,11 @@
+﻿using InterviewSystem.Domain.Common.Enums;
+using System.ComponentModel.DataAnnotations;
+
+namespace InterviewSystem.Application.DTOs.Candidate;
+
+public record CandidateCreateDTO(
+    [Required, StringLength(100, MinimumLength = 1)] string Name,
+    [Required, StringLength(100, MinimumLength = 1), EmailAddress] string Email,
+    [Required, StringLength(20, MinimumLength = 1)] string PhoneNumber,
+    [Required] AppliedPosition AppliedPosition
+    );
---
+++ Projects/interview-system/InterviewSystem.Application/DTOs/Candidate/CandidateReadDTO.cs
@@ -0,0 +1,11 @@
+﻿using InterviewSystem.Domain.Common.Enums;
+
+namespace InterviewSystem.Application.DTOs.Candidate;
+
+public record CandidateReadDTO(
+    Guid Id,
+    string Name,
+    string Email,
+    string PhoneNumber,
+    AppliedPosition AppliedPosition
+    );
