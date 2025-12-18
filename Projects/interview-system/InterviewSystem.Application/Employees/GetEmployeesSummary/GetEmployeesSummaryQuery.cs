c9eebe46bd6e0693cfc669ca40c02925bc1d9b21
zulhamzakaria <m.zulham.zakaria@gmail.com>
2025-12-19 07:44:20 +0800

    - additional args for Querying

--- Projects/interview-system/InterviewSystem.Application/Employees/GetEmployeesSummary/GetEmployeesSummaryQuery.cs
+++ Projects/interview-system/InterviewSystem.Application/Employees/GetEmployeesSummary/GetEmployeesSummaryQuery.cs
@@ -2,4 +2,9 @@
 
 namespace InterviewSystem.Application.Employees.GetEmployeesSummary;
 
-public sealed record GetEmployeesSummaryQuery(EmployeeDepartment? Department);
+public sealed record GetEmployeesSummaryQuery(
+    EmployeeType? EmployeeType = null,
+    EmployeeDepartment? EmployeeDepartment = null,
+    EmployeePosition? EmployeePosition = null,
+    EmployeeStatus? EmployeeStatus = null
+    );
