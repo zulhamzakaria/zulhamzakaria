d30ff53de5f186aaa816d9239381b145f552fb7b
zulhamzakaria <m.zulham.zakaria@gmail.com>
2025-12-18 18:24:28 +0800

    - artefacts - Command is connected to Repo - DTO is connected to API

--- Projects/interview-system/InterviewSystem.Application/Employees/CreateEmployee/CreateEmployeeDTO.cs
+++ Projects/interview-system/InterviewSystem.Application/Employees/CreateEmployee/CreateEmployeeDTO.cs
@@ -1,12 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using InterviewSystem.Domain.Common.Enums;
 
-namespace InterviewSystem.Application.Employees.CreateEmployee
-{
-    internal class CreateEmployeeDTO
-    {
-    }
-}
+namespace InterviewSystem.Application.Employees.CreateEmployee;
+
+public sealed record CreateEmployeeRequestDto(
+    string Name,
+    string Email,
+    EmployeeType EmployeeType,
+    EmployeeDepartment EmployeeDepartment,
+    EmployeePosition EmployeePosition
+);
