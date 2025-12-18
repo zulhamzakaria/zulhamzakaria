e31717b5238ffc75df3d9fe11f910e0c130bd22d
zulhamzakaria <m.zulham.zakaria@gmail.com>
2025-12-18 18:31:21 +0800

    - "Orchestrator" for an action - WIP

--- Projects/interview-system/InterviewSystem.Application/Employees/CreateEmployee/CreateEmployeeHandler.cs
+++ Projects/interview-system/InterviewSystem.Application/Employees/CreateEmployee/CreateEmployeeHandler.cs
@@ -1,12 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace InterviewSystem.Application.Employees.CreateEmployee;
 
-namespace InterviewSystem.Application.Employees.CreateEmployee
+public sealed class CreateEmployeeHandler
 {
-    internal class CreateEmployeeHandler
-    {
-    }
 }
