462c842ff1004a834946bee7439bc15f00411be6
zulhamzakaria <m.zulham.zakaria@gmail.com>
2025-12-18 18:36:59 +0800

    - wIP

--- Projects/interview-system/InterviewSystem.Application/Employees/CreateEmployee/CreateEmployeeHandler.cs
+++ Projects/interview-system/InterviewSystem.Application/Employees/CreateEmployee/CreateEmployeeHandler.cs
@@ -1,5 +1,20 @@
-﻿namespace InterviewSystem.Application.Employees.CreateEmployee;
+﻿using InterviewSystem.Domain.Common.ErrorHandling;
+using InterviewSystem.Domain.Interfaces.Repositories;
+
+namespace InterviewSystem.Application.Employees.CreateEmployee;
 
 public sealed class CreateEmployeeHandler
 {
+    private readonly IUnitOfWorkRepository _uow;
+    private readonly IEmployeeRepository _employeeRepository;
+    public CreateEmployeeHandler(IUnitOfWorkRepository uow, IEmployeeRepository employeeRepository)
+    {
+        _uow = uow;
+        _employeeRepository = employeeRepository;
+    }
+
+    public async Task<Result<Guid>> HandleAsync(CreateEmployeeCommand command)
+    {
+        var employeeResult = 
+    }
 }
