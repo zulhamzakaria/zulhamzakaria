2af29256f5e6e68cb6de0df3d6c4dc027ea846e4
zulhamzakaria <m.zulham.zakaria@gmail.com>
2025-12-20 18:40:12 +0800

    - CreateInterviewRound action - WIP

--- Projects/interview-system/InterviewSystem.Application/InterviewRounds/CreateInterviewRound/Command.cs
+++ Projects/interview-system/InterviewSystem.Application/InterviewRounds/CreateInterviewRound/Command.cs
@@ -1,12 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using InterviewSystem.Domain.Common.Enums;
 
-namespace InterviewSystem.Application.InterviewRounds.CreateInterviewRound
-{
-    internal class Command
-    {
-    }
-}
+namespace InterviewSystem.Application.InterviewRounds.CreateInterviewRound;
+
+public sealed record Command(EmployeeDepartment EmployeeDepartment);
