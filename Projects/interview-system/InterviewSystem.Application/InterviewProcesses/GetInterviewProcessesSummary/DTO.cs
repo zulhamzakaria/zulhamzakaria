56fb0c6b60dbe5b8fd3b14945b7d116e0b1583ce
zulhamzakaria <m.zulham.zakaria@gmail.com>
2025-12-20 17:51:36 +0800

    - InterviewProcessSummary action

--- Projects/interview-system/InterviewSystem.Application/InterviewProcesses/GetInterviewProcessesSummary/DTO.cs
+++ Projects/interview-system/InterviewSystem.Application/InterviewProcesses/GetInterviewProcessesSummary/DTO.cs
@@ -1,12 +1,3 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace InterviewSystem.Application.InterviewProcesses.GetInterviewProcessesSummary;
 
-namespace InterviewSystem.Application.InterviewProcesses.GetInterviewProcessesSummary
-{
-    internal class DTO
-    {
-    }
-}
+public sealed record DTO();
