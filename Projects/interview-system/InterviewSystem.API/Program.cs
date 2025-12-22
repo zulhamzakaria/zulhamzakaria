045744815df48f52231e22030cedfd901d8a6b80
zulhamzakaria <m.zulham.zakaria@gmail.com>
2025-12-22 14:37:55 +0800

    - added InterviewTaskRepo DI container

--- Projects/interview-system/InterviewSystem.API/Program.cs
+++ Projects/interview-system/InterviewSystem.API/Program.cs
@@ -20,8 +20,9 @@ builder.Services.AddDbContext<AppDbContext>(options =>
 builder.Services.AddScoped<IUnitOfWorkRepository, UnitOfWorkRepository>();
 builder.Services.AddScoped<IEmployeeRepository, EmployeeRepository>();
 builder.Services.AddScoped<ICandidateRepository, CandidateRepository>();
-builder.Services.AddScoped<IInterviewProcessRepository, InterviewProcessRepository>();
+builder.Services.AddScoped<IInterviewTaskRepository, InterviewTaskRepository>();
 builder.Services.AddScoped<IInterviewRoundRepository, InterviewRoundRepository>();
+builder.Services.AddScoped<IInterviewProcessRepository, InterviewProcessRepository>();
 
 var app = builder.Build();
 
