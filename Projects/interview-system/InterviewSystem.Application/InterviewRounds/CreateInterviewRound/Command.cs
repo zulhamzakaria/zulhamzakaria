a9e56f635825a48e0bb06cc8fc7d2bb01e85f1df
zulhamzakaria <m.zulham.zakaria@gmail.com>
2025-12-22 09:39:37 +0800

    - Create interviewround command

--- Projects/interview-system/InterviewSystem.Application/InterviewRounds/CreateInterviewRound/Command.cs
+++ Projects/interview-system/InterviewSystem.Application/InterviewRounds/CreateInterviewRound/Command.cs
@@ -1,5 +1,10 @@
 ﻿using InterviewSystem.Domain.Common.Enums;
+using System.ComponentModel.DataAnnotations;
 
 namespace InterviewSystem.Application.InterviewRounds.CreateInterviewRound;
 
-public sealed record Command(EmployeeDepartment EmployeeDepartment);
+public sealed record Command(
+    [Required] EmployeeDepartment EmployeeDepartment,
+    [Required] AppliedPosition AppliedPosition,
+    [Required] IReadOnlyList<EmployeePosition> EmployeePositions
+    );
