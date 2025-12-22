a197ed83e3c193e9c0619b408c5eb0d072683b61
zulhamzakaria <m.zulham.zakaria@gmail.com>
2025-12-22 14:31:11 +0800

    - CreateInterviewTask action - WIP

--- Projects/interview-system/InterviewSystem.Application/InterviewTasks/CreateInterviewTask/Command.cs
+++ Projects/interview-system/InterviewSystem.Application/InterviewTasks/CreateInterviewTask/Command.cs
@@ -1,12 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using InterviewSystem.Domain.Common.Enums;
+using InterviewSystem.Domain.Entity;
+using System.ComponentModel.DataAnnotations;
 
-namespace InterviewSystem.Application.InterviewTasks.CreateInterviewTask
-{
-    internal class Command
-    {
-    }
-}
+namespace InterviewSystem.Application.InterviewTasks.CreateInterviewTask;
+
+public sealed record Command(
+    [Required] Guid InterviewRoundId,
+    [Required] Guid CandidateId,
+    [Required] InterviewTaskStatus InterviewTaskStatus,
+    [Required] DateTimeOffset AssignedAt,
+    [Required] DateTimeOffset? CompletedAt,
+    [Required] CandidateEvaluation CandidateEvaluation,
+    bool Rejected,
+    string? TaskRejectionReason
+    );
