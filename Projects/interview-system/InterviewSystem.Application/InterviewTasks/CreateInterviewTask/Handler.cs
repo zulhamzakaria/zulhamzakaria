e76b5b6dc13364832ab65fea4894f15bdfff49ff
zulhamzakaria <m.zulham.zakaria@gmail.com>
2025-12-22 14:44:51 +0800

    - CreateInterview task action

--- Projects/interview-system/InterviewSystem.Application/InterviewTasks/CreateInterviewTask/Handler.cs
+++ Projects/interview-system/InterviewSystem.Application/InterviewTasks/CreateInterviewTask/Handler.cs
@@ -1,12 +1,37 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using InterviewSystem.Domain.Common.ErrorHandling;
+using InterviewSystem.Domain.Entity;
+using InterviewSystem.Domain.Interfaces.Repositories;
 
-namespace InterviewSystem.Application.InterviewTasks.CreateInterviewTask
+namespace InterviewSystem.Application.InterviewTasks.CreateInterviewTask;
+
+public sealed class Handler
 {
-    internal class Handler
+    private readonly IUnitOfWorkRepository _uow;
+    private readonly IInterviewTaskRepository _interviewTaskRepository;
+
+    public Handler(IInterviewTaskRepository interviewTaskRepository, IUnitOfWorkRepository uow)
     {
+        _interviewTaskRepository = interviewTaskRepository;
+        _uow = uow;
+    }
+
+    public async Task<Result<Guid>> HandleAsync(Command command)
+    {
+        var result =  InterviewTask.Create(
+            command.InterviewRoundId, 
+            command.CandidateId, 
+            command.Rejected, 
+            command.TaskRejectionReason);
+
+        if (result.IsFailure)
+            return Result<Guid>.Failure(result.Errors);
+
+        var newTask = result.Value;
+
+        await _interviewTaskRepository.AddAsync(newTask);
+        await _uow.SaveChangesAsync();
+
+        return Result<Guid>.Success(newTask.Id);
+
     }
 }
