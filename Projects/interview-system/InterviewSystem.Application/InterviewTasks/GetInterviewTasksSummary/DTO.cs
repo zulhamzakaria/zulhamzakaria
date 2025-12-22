79390ed12be9420a44a183563eb900911bba638d
zulhamzakaria <m.zulham.zakaria@gmail.com>
2025-12-22 16:41:05 +0800

    - WIP

--- Projects/interview-system/InterviewSystem.Application/InterviewTasks/GetInterviewTasksSummary/DTO.cs
+++ Projects/interview-system/InterviewSystem.Application/InterviewTasks/GetInterviewTasksSummary/DTO.cs
@@ -1,12 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using InterviewSystem.Domain.Common.Enums;
 
-namespace InterviewSystem.Application.InterviewTasks.GetInterviewTasksSummary
-{
-    internal class DTO
-    {
-    }
-}
+namespace InterviewSystem.Application.InterviewTasks.GetInterviewTasksSummary;
+
+public sealed record DTO(
+    Guid Id,
+    string CandidateName,
+    EmployeeDepartment EmployeeDepartment,
+    AppliedPosition AppliedPosition,
+    InterviewTaskStatus InterviewTaskStatus,
+    DateTimeOffset AssignedAt,
+    bool Rejected
+    );
