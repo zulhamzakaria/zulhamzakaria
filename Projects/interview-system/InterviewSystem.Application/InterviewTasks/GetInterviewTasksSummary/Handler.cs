c04a6e0ac06f5877ee2b43b4c32d356a70e10821
zulhamzakaria <m.zulham.zakaria@gmail.com>
2025-12-23 10:21:10 +0800

    - customquery action

--- Projects/interview-system/InterviewSystem.Application/InterviewTasks/GetInterviewTasksSummary/Handler.cs
+++ Projects/interview-system/InterviewSystem.Application/InterviewTasks/GetInterviewTasksSummary/Handler.cs
@@ -1,4 +1,6 @@
 ﻿using InterviewSystem.Domain.Common.ErrorHandling;
+using InterviewSystem.Domain.Common.ErrorHandling.Errors;
+using InterviewSystem.Domain.Entity;
 using InterviewSystem.Domain.Interfaces.Repositories;
 
 namespace InterviewSystem.Application.InterviewTasks.GetInterviewTasksSummary;
@@ -6,18 +8,27 @@ namespace InterviewSystem.Application.InterviewTasks.GetInterviewTasksSummary;
 public sealed class Handler
 {
     private readonly IInterviewTaskRepository _interviewTaskRepository;
+    private readonly IInterviewTaskQueryRepository _interviewTaskQueryRepository;
 
-    private readonly IInterviewRoundRepository _interviewRoundRepository;
-
-    public Handler(IInterviewRoundRepository interviewRoundRepository,
-        IInterviewTaskRepository interviewTaskRepository)
+    public Handler(IInterviewTaskRepository interviewTaskRepository,
+        IInterviewTaskQueryRepository interviewTaskQueryRepository)
     {
-        _interviewRoundRepository = interviewRoundRepository;
         _interviewTaskRepository = interviewTaskRepository;
+        _interviewTaskQueryRepository = interviewTaskQueryRepository;
     }
 
-    public async Task<Result<IReadOnlyCollection<DTO>>> HandleAsync(Query query)
+    public async Task<Result<IReadOnlyCollection<InterviewTasksSummaryDTO>>> HandleAsync(Query query)
     {
-        throw new Exception();
+        var results = (await _interviewTaskQueryRepository.GetSummaryAsync())
+            .Where(cq => query.EmployeeDepartment == null || cq.EmployeeDepartment == query.EmployeeDepartment)
+            .Where(cq => query.AppliedPosition == null || cq.AppliedPosition == query.AppliedPosition)
+            .Where(cq => query.InterviewTaskStatus == null || cq.InterviewTaskStatus == query.InterviewTaskStatus)
+            .ToList();
+
+        if (results.Any() is false)
+            return Result<IReadOnlyCollection<InterviewTasksSummaryDTO>>
+                .Failure(GenericErrors.NoRecordsFound(nameof(InterviewTask)));
+
+        return Result<IReadOnlyCollection<InterviewTasksSummaryDTO>>.Success(results);
     }
 }
