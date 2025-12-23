b73fcb9be6cb4ffd49f76ce9360e740b65290329
zulhamzakaria <m.zulham.zakaria@gmail.com>
2025-12-24 01:34:15 +0800

    - extension method for Result<T> to eliminate repeating result.IsFailure

---
+++ Projects/interview-system/InterviewSystem.API/Controllers/CandidateController.cs
@@ -0,0 +1,26 @@
+﻿using InterviewSystem.API.ErrorHandling;
+using InterviewSystem.Application.Candidates.CreateCandidate;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace InterviewSystem.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CandidateController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public CandidateController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateCandidate([FromBody] CreateCandidateCommand command)
+        {
+            var result = await _mediator.Send(command);
+            return result.ToActionResult();
+        }
+    }
+}
---
+++ Projects/interview-system/InterviewSystem.API/ErrorHandling/ResultExtensions.cs
@@ -0,0 +1,22 @@
+﻿using InterviewSystem.Domain.Common.Enums;
+using InterviewSystem.Domain.Common.ErrorHandling;
+using Microsoft.AspNetCore.Mvc;
+
+namespace InterviewSystem.API.ErrorHandling;
+
+public static class ResultExtensions
+{
+    public static IActionResult ToActionResult<T>(this Result<T> result)
+    {
+        if (result.IsSuccess)
+            return new OkObjectResult(result.Value);
+
+        return result.Errors.FirstOrDefault()?.ErrorType switch
+        {
+            ErrorType.Validation => new BadRequestObjectResult(result.Errors),
+            ErrorType.BusinessRule => new ConflictObjectResult(result.Errors),
+            _ => new ObjectResult(result.Errors) { StatusCode = StatusCodes.Status500InternalServerError }
+        };
+
+    }
+}
