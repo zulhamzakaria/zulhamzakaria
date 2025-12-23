48af5ced7f586c96586fdced70bbac9c720812a6
zulhamzakaria <m.zulham.zakaria@gmail.com>
2025-12-23 19:43:29 +0800

    - added ErrorTypeToTheErrorClass

--- Projects/interview-system/InterviewSystem.Domain/Common/ErrorHandling/Errors/Error.cs
+++ Projects/interview-system/InterviewSystem.Domain/Common/ErrorHandling/Errors/Error.cs
@@ -1,12 +1,16 @@
-﻿namespace InterviewSystem.Domain.Common.ErrorHandling.Errors;
+﻿using InterviewSystem.Domain.Common.Enums;
+
+namespace InterviewSystem.Domain.Common.ErrorHandling.Errors;
 
 public sealed record Error
 {
     public string Code { get; } = string.Empty;
     public string Message { get; } = string.Empty;
+    public ErrorType ErrorType { get; }
 
-    public Error(string code, string message)
+    public Error(ErrorType errorType, string code, string message)
     {
+        ErrorType = errorType;
         Code = code;
         Message = message;
     }
