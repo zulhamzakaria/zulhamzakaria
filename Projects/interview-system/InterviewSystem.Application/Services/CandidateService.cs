1b6c5fc4f4698a49bd3286de9e7b6ef02b2dda01
zulhamzakaria <m.zulham.zakaria@gmail.com>
2025-12-23 14:41:10 +0800

    - template for Service - WIP

---
+++ Projects/interview-system/InterviewSystem.Application/Services/CandidateService.cs
@@ -0,0 +1,17 @@
+﻿using InterviewSystem.Application.Candidates.CreateCandidate;
+
+namespace InterviewSystem.Application.Services;
+
+public class CandidateService
+{
+    private readonly Handler _handler;
+    public CandidateService(Handler handler)
+    {
+        _handler = handler;
+    }
+
+    public void Test(Command cmd)
+    {
+        var result = _handler.HandleAsync(cmd);
+    }
+}
