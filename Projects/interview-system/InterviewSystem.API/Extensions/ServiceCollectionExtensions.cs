d2de431f572da3d1663ac9c6310229a6ef7f28b8
zulhamzakaria <m.zulham.zakaria@gmail.com>
2025-12-24 08:51:09 +0800

    - external extension method for registering mediatr services

---
+++ Projects/interview-system/InterviewSystem.API/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,14 @@
+﻿using InterviewSystem.Application.Candidates.CreateCandidate;
+
+namespace InterviewSystem.API.Extensions;
+
+public static class ServiceCollectionExtensions
+{
+    public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+    {
+        //Candidate
+        services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(typeof(CreateCandidateCommand).Assembly));
+
+        return services;
+    }
+}
