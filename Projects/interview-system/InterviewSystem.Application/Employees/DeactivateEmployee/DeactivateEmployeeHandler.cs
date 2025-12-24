7f47d5f08f17489278feb80c142af771a4165715
zulhamzakaria <m.zulham.zakaria@gmail.com>
2025-12-25 07:18:17 +0800

    - implementation of DeactivateEmployee

--- Projects/interview-system/InterviewSystem.Application/Employees/DeactivateEmployee/DeactivateEmployeeHandler.cs
+++ Projects/interview-system/InterviewSystem.Application/Employees/DeactivateEmployee/DeactivateEmployeeHandler.cs
@@ -10,22 +10,27 @@ namespace InterviewSystem.Application.Employees.DeactivateEmployee;
 public sealed class DeactivateEmployeeHandler : IRequestHandler<DeactivateEmployeeCommand, Result<Unit>>
 {
     private readonly IEmployeeRepository _employeeRepository;
+    private readonly IInterviewTaskRepository _interviewTaskRepository;
     private readonly IUnitOfWorkRepository _uow;
 
-    public DeactivateEmployeeHandler(IEmployeeRepository employeeRepository, IUnitOfWorkRepository uow)
+    public DeactivateEmployeeHandler(IEmployeeRepository employeeRepository, IUnitOfWorkRepository uow, 
+        IInterviewTaskRepository interviewTaskRepository)
     {
         _employeeRepository = employeeRepository;
         _uow = uow;
+        _interviewTaskRepository = interviewTaskRepository;
     }
 
     public async Task<Result<Unit>> Handle(DeactivateEmployeeCommand request, CancellationToken cancellationToken)
     {
         var employee = await _employeeRepository.GetByIdAsync(request.employeeId);
-
         if (employee is null)
             return Result<Unit>.Failure(GenericErrors.NoRecordFound(nameof(Employee), request.employeeId));
 
         //Pending task
+        var tasks = await _interviewTaskRepository.GetAllByEmployeeId(request.employeeId);
+        if (tasks.Any())
+            return Result<Unit>.Failure(InterviewTaskErrors.PendingTasks());
 
         employee.Deactivate();
         await _uow.SaveChangesAsync();
---
+++ Projects/interview-system/InterviewSystem.Domain/Common/ErrorHandling/Errors/InterviewTaskErrors.cs
@@ -0,0 +1,9 @@
+﻿using InterviewSystem.Domain.Common.Enums;
+
+namespace InterviewSystem.Domain.Common.ErrorHandling.Errors;
+
+public static class InterviewTaskErrors
+{
+    public static Error PendingTasks()
+        => new(ErrorType.Validation, "ACTIVE_PENDING_TASKS", "Cannot Deactivate Employee with Active Pending Tasks");
+}
