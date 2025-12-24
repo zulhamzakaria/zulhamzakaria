24c92528c64f639b215ca75deec4bc7f149ea3de
zulhamzakaria <m.zulham.zakaria@gmail.com>
2025-12-25 06:51:13 +0800

    - removed InterviewRoundId relationship from the db

--- Projects/interview-system/InterviewSystem.Infrastructure/AppDbContext.cs
+++ Projects/interview-system/InterviewSystem.Infrastructure/AppDbContext.cs
@@ -52,11 +52,6 @@ public class AppDbContext : DbContext
             .HasForeignKey(x => x.CandidateId)
             .OnDelete(DeleteBehavior.Restrict);
 
-        modelBuilder.Entity<InterviewTask>()
-            .HasOne<InterviewRound>()
-            .WithMany()
-            .HasForeignKey(x => x.InterviewRoundId)
-            .OnDelete(DeleteBehavior.Restrict);
     }
 
     private void ConfigureInterviewRound(ModelBuilder modelBuilder)
