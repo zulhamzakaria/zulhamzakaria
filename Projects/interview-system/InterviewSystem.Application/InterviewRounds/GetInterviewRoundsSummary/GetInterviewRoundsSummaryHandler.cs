fd917319f62901b17fe80d9e02f8c2bdb90858a7
zulhamzakaria <m.zulham.zakaria@gmail.com>
2025-12-25 12:05:32 +0800

    - applied mediatr

--- Projects/interview-system/InterviewSystem.Application/InterviewRounds/GetInterviewRoundsSummary/GetInterviewRoundsSummaryHandler.cs
+++ Projects/interview-system/InterviewSystem.Application/InterviewRounds/GetInterviewRoundsSummary/GetInterviewRoundsSummaryHandler.cs
@@ -2,10 +2,12 @@
 using InterviewSystem.Domain.Common.ErrorHandling.Errors;
 using InterviewSystem.Domain.Entity;
 using InterviewSystem.Domain.Interfaces.Repositories;
+using MediatR;
 
 namespace InterviewSystem.Application.InterviewRounds.GetInterviewRoundsSummary;
 
-public sealed class GetInterviewRoundsSummaryHandler
+public sealed class GetInterviewRoundsSummaryHandler :
+    IRequestHandler<GetInterviewRoundsSummaryQuery, Result<IReadOnlyCollection<GetInterviewRoundsSummaryDTO>>>
 {
     private readonly IInterviewRoundRepository _interviewRoundRepository;
 
@@ -14,15 +16,16 @@ public sealed class GetInterviewRoundsSummaryHandler
         _interviewRoundRepository = interviewRoundRepository;
     }
 
-    public async Task<Result<IReadOnlyCollection<GetInterviewRoundsSummaryDTO>>> HandleAsync(GetInterviewRoundsSummaryQuery query)
+    public async Task<Result<IReadOnlyCollection<GetInterviewRoundsSummaryDTO>>> Handle
+        (GetInterviewRoundsSummaryQuery request, CancellationToken cancellationToken)
     {
         var results = (await _interviewRoundRepository.GetAllAsync())
-            .Where(ir => query.EmployeeDepartment == null || ir.Department == query.EmployeeDepartment)
-            .Where(ir => query.AppliedPosition == null || ir.AppliedPosition == query.AppliedPosition)
-            .Select(MapToDTO)
-            .ToList();
+           .Where(ir => request.EmployeeDepartment == null || ir.Department == request.EmployeeDepartment)
+           .Where(ir => request.AppliedPosition == null || ir.AppliedPosition == request.AppliedPosition)
+           .Select(MapToDTO)
+           .ToList();
 
-        if(results.Any() is false)
+        if (results.Any() is false)
             return Result<IReadOnlyCollection<GetInterviewRoundsSummaryDTO>>.Failure(GenericErrors.NoRecordsFound(nameof(InterviewRound)));
 
         return Result<IReadOnlyCollection<GetInterviewRoundsSummaryDTO>>.Success(results);
--- Projects/interview-system/InterviewSystem.Application/InterviewRounds/GetInterviewRoundsSummary/GetInterviewRoundsSummaryQuery.cs
+++ Projects/interview-system/InterviewSystem.Application/InterviewRounds/GetInterviewRoundsSummary/GetInterviewRoundsSummaryQuery.cs
@@ -1,8 +1,10 @@
 ﻿using InterviewSystem.Domain.Common.Enums;
+using InterviewSystem.Domain.Common.ErrorHandling;
+using MediatR;
 
 namespace InterviewSystem.Application.InterviewRounds.GetInterviewRoundsSummary;
 
 public sealed record GetInterviewRoundsSummaryQuery(
     EmployeeDepartment? EmployeeDepartment,
     AppliedPosition? AppliedPosition
-    );
+    ) : IRequest<Result<IReadOnlyCollection<GetInterviewRoundsSummaryDTO>>> ;
