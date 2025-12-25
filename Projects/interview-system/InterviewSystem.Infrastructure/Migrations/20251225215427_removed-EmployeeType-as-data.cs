4d74e3d5403a2a163626205f09486f003c782e2f
zulhamzakaria <m.zulham.zakaria@gmail.com>
2025-12-26 05:54:51 +0800

    - migrations

---
+++ Projects/interview-system/InterviewSystem.Infrastructure/Migrations/20251225215427_removed-EmployeeType-as-data.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace InterviewSystem.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class removedEmployeeTypeasdata : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "EmployeeType",
+                table: "Employees");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "EmployeeType",
+                table: "Employees",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
