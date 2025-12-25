421b8017be84db0a790e69cc2aea03a1051f43c2
zulhamzakaria <m.zulham.zakaria@gmail.com>
2025-12-25 13:08:08 +0800

    - WIP

---
+++ Projects/interview-system/InterviewSystem.API/Controllers/InterviewRoundController.cs
@@ -0,0 +1,26 @@
+﻿using InterviewSystem.API.ErrorHandling;
+using InterviewSystem.Application.InterviewRounds.GetInterviewRoundsSummary;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace InterviewSystem.API.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class InterviewRoundController : ControllerBase
+{
+    private readonly IMediator _mediator;
+
+    public InterviewRoundController(IMediator mediator)
+    {
+        _mediator = mediator;
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetInterviewRounds
+        ([FromQuery] GetInterviewRoundsSummaryQuery query)
+    {
+        var result = await _mediator.Send(query);
+        return result.ToActionResult();
+    }
+}
