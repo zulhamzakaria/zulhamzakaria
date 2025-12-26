13425294a7c3b3f3cffd8d19f379cde99d627e4e
zulhamzakaria <m.zulham.zakaria@gmail.com>
2025-12-26 17:48:14 +0800

    - migration

---
+++ Projects/interview-system/InterviewSystem.Domain/Entity/InterviewRoundRules.cs
@@ -0,0 +1,5 @@
+﻿namespace InterviewSystem.Domain.Entity;
+
+public static class InterviewRoundRules
+{
+}
---
+++ Projects/interview-system/InterviewSystem.Infrastructure/Migrations/20251226063616_added-EmployeeLevel-to-Employee.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace InterviewSystem.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class addedEmployeeLeveltoEmployee : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "EmployeeLevel",
+                table: "Employees",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "EmployeeLevel",
+                table: "Employees");
+        }
+    }
+}
