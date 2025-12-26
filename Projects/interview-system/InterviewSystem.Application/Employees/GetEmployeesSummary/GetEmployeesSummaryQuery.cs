9f6a92097e6dbe2d5f320227634605651b8208c4
zulhamzakaria <m.zulham.zakaria@gmail.com>
2025-12-26 11:31:36 +0800

    - removed EmployeeType reference wince we're using EmployeeRules now

--- Projects/interview-system/InterviewSystem.Application/Employees/GetEmployeesSummary/GetEmployeesSummaryQuery.cs
+++ Projects/interview-system/InterviewSystem.Application/Employees/GetEmployeesSummary/GetEmployeesSummaryQuery.cs
@@ -5,7 +5,6 @@ using MediatR;
 namespace InterviewSystem.Application.Employees.GetEmployeesSummary;
 
 public sealed record GetEmployeesSummaryQuery(
-    EmployeeType? EmployeeType = null,
     EmployeeDepartment? EmployeeDepartment = null,
     EmployeePosition? EmployeePosition = null,
     EmployeeStatus? EmployeeStatus = null
