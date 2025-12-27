7d569a22d5da9fbea1897453e0c93971ceff08fb
zulhamzakaria <m.zulham.zakaria@gmail.com>
2025-12-28 07:23:59 +0800

    - Erorr for undefined initiator - ceremonial?

--- Projects/interview-system/InterviewSystem.Application/InterviewProcesses/CreateInterviewProcess/CreateInterviewProcessHandler.cs
+++ Projects/interview-system/InterviewSystem.Application/InterviewProcesses/CreateInterviewProcess/CreateInterviewProcessHandler.cs
@@ -1,4 +1,5 @@
-﻿using InterviewSystem.Domain.Common.ErrorHandling;
+﻿using InterviewSystem.Domain.Common.Enums;
+using InterviewSystem.Domain.Common.ErrorHandling;
 using InterviewSystem.Domain.Common.ErrorHandling.Errors;
 using InterviewSystem.Domain.Entity;
 using InterviewSystem.Domain.Helpers;
@@ -66,7 +67,7 @@ public sealed class CreateInterviewProcessHandler : IRequestHandler<CreateInterv
         var employeePosition = roundPolicy.Steps.FirstOrDefault();
 
         if (employeePosition is null)
-            return Result<Guid>.Failure();
+            return Result<Guid>.Failure(InterviewRoundErrors.UndefinedPolicy(EmployeePosition.HiringManager));
 
         var employees = await _employeeRepository.GetEmployeesByPositionAsync
             (employeePosition.AllowedPositions.FirstOrDefault());
--- Projects/interview-system/InterviewSystem.Domain/Common/ErrorHandling/Errors/InterviewRoundErrors.cs
+++ Projects/interview-system/InterviewSystem.Domain/Common/ErrorHandling/Errors/InterviewRoundErrors.cs
@@ -7,4 +7,7 @@ public static class InterviewRoundErrors
     public static Error UndefinedPolicy(AppliedPosition appliedPosition)
         => new(ErrorType.Validation, "POLICY_NOT_DEFINED", 
             $"No InterviewRound Policy has been defined for {appliedPosition.ToString()}");
+    public static Error UndefinedInitiator()
+        => new(ErrorType.Validation, "INITIATOR_NOT_DEFINED",
+            $"No Initiator has been defined for the Policy");
 }
