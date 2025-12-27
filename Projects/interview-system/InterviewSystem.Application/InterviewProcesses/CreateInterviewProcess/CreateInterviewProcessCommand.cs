a8b137495e149b92202a81a497dc6920105c528e
zulhamzakaria <m.zulham.zakaria@gmail.com>
2025-12-27 09:26:18 +0800

    - simplified the Command since all of the values can be gotten from db

--- Projects/interview-system/InterviewSystem.Application/InterviewProcesses/CreateInterviewProcess/CreateInterviewProcessCommand.cs
+++ Projects/interview-system/InterviewSystem.Application/InterviewProcesses/CreateInterviewProcess/CreateInterviewProcessCommand.cs
@@ -1,15 +1,7 @@
-﻿using InterviewSystem.Domain.Common.Enums;
-using InterviewSystem.Domain.Common.ErrorHandling;
+﻿using InterviewSystem.Domain.Common.ErrorHandling;
 using MediatR;
 using System.ComponentModel.DataAnnotations;
 
 namespace InterviewSystem.Application.InterviewProcesses.CreateInterviewProcess;
 
-public sealed record CreateInterviewProcessCommand(
-    [Required] Guid CandidateId,
-    [Required] string CandidateName,
-    [Required] EmployeeDepartment EmployeeDepartment,
-    [Required] int CurrentSequence,
-    [Required] InterviewProcessStatus InterviewProcessStatus,
-    Guid CurrentInterviewId, //inserted by system?
-    string CurrentInterviewName) : IRequest<Result<Guid>>; //inserted by system?
+public sealed record CreateInterviewProcessCommand([Required] Guid CandidateId) : IRequest<Result<Guid>>;
