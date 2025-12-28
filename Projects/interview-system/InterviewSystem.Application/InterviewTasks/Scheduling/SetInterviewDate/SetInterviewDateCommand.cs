c34e8b65ec99467a39ffc4ddd0c24cc0145acd68
zulhamzakaria <m.zulham.zakaria@gmail.com>
2025-12-29 05:16:33 +0800

    - template for the Command and Handler - WIP

---
+++ Projects/interview-system/InterviewSystem.Application/InterviewTasks/Scheduling/SetInterviewDate/SetInterviewDateCommand.cs
@@ -0,0 +1,10 @@
+﻿using InterviewSystem.Domain.Common.ErrorHandling;
+using MediatR;
+
+namespace InterviewSystem.Application.InterviewTasks.Scheduling.SetInterviewDate;
+
+public sealed record SetInterviewDateCommand(
+    Guid AssigneeId,
+    Guid TaskId,
+    DateTimeOffset InterviewDate
+    ) : IRequest<Result<DateTimeOffset>>;
---
+++ Projects/interview-system/InterviewSystem.Application/InterviewTasks/Scheduling/SetInterviewDate/SetInterviewDateHandler.cs
@@ -0,0 +1,20 @@
+﻿using InterviewSystem.Domain.Common.ErrorHandling;
+using InterviewSystem.Domain.Interfaces.Repositories;
+using MediatR;
+
+namespace InterviewSystem.Application.InterviewTasks.Scheduling.SetInterviewDate;
+
+public sealed class SetInterviewDateHandler : IRequestHandler<SetInterviewDateCommand, Result<DateTimeOffset>>
+{
+    private readonly IInterviewTaskRepository _interviewTaskRepository;
+    private readonly IUnitOfWorkRepository _uow;
+    public SetInterviewDateHandler(IUnitOfWorkRepository uow, IInterviewTaskRepository interviewTaskRepository)
+    {
+        _uow = uow;
+        _interviewTaskRepository = interviewTaskRepository;
+    }
+    public Task<Result<DateTimeOffset>> Handle(SetInterviewDateCommand request, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+}
