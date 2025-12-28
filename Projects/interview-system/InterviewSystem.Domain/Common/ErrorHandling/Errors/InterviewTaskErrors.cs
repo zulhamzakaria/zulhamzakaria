18eaac15a7d98d4223e0b7eb4bc39f4192696b2e
zulhamzakaria <m.zulham.zakaria@gmail.com>
2025-12-29 04:36:28 +0800

    - InterviewTask Scheduling errors

--- Projects/interview-system/InterviewSystem.Domain/Common/ErrorHandling/Errors/InterviewTaskErrors.cs
+++ Projects/interview-system/InterviewSystem.Domain/Common/ErrorHandling/Errors/InterviewTaskErrors.cs
@@ -6,4 +6,12 @@ public static class InterviewTaskErrors
 {
     public static Error PendingTasks()
         => new(ErrorType.Validation, "ACTIVE_PENDING_TASKS", "Cannot Deactivate Employee with Active Pending Tasks");
+    public static Error BackdatedInterviewDate()
+        => new(ErrorType.Validation, "INTERVIEW_DATE_BACKDATED", "Cannot set the Interview Date before current date");
+    public static Error InterviewDateTooFar()
+        => new(ErrorType.BusinessRule, "INTERVIEW_DATE_TOO_FAR", "Cannot set the Interview Date too far in the future");
+    public static Error InterviewDateExists()
+        => new(ErrorType.BusinessRule, "INTERVIEW_DATE_EXISTS", "Interview Date has been set. Please use Reschedule instead");
+    public static Error InvalidScheduling()
+        => new(ErrorType.Validation, "INTERVIEW_DATE_INVALID", "Cannot set Interview Date on Saturday or Sunday and outside working hours");
 }
