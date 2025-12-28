61060d0c215c9882ad689d49950e854198e13835
zulhamzakaria <m.zulham.zakaria@gmail.com>
2025-12-28 12:00:27 +0800

    - dropped NumberOfRounds manually

---
+++ Projects/interview-system/InterviewSystem.Infrastructure/Migrations/20251228035742_remove-number-of-rounds.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace InterviewSystem.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class removenumberofrounds : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "NumberOfRounds",
+                table: "InterviewRounds");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "NumberOfRounds",
+                table: "InterviewRounds",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+        }
+    }
+}
