618ee076381411f575e273ecedf3f0fb12fe70c0
zulhamzakaria <m.zulham.zakaria@gmail.com>
2025-12-29 12:40:43 +0800

    - bg job for scheduled interview tasks

--- Projects/interview-system/InterviewSystem.API/BackgroundJobs/InterviewReminder.cs
+++ Projects/interview-system/InterviewSystem.API/BackgroundJobs/InterviewReminder.cs
@@ -20,15 +20,29 @@ public sealed class InterviewReminder : BackgroundService
         {
             try
             {
-                await CheckPendingInterviews(stoppingToken);
+                var now = DateTimeOffset.UtcNow;
+                var nextRun = now.Date.AddDays(1);
+                var delay = nextRun - now;
+
+                await Task.Delay(delay, stoppingToken);
+
+                int maxRetries = 5;
+
+                await RetryAsync(
+                     async() => await CheckPendingInterviews(stoppingToken),
+                     maxRetries,
+                     TimeSpan.FromMinutes(5),
+                     stoppingToken);
+            }
+            catch (TaskCanceledException ex)
+            {
+                _logger.LogError(ex.InnerException?.Message ?? ex.Message);
             }
             catch (Exception ex)
             {
                 _logger.LogError($"failed to execute InterviewReminder background job: " +
                     $"{ex.InnerException?.Message ?? ex.Message}");
             }
-
-            await Task.Delay(TimeSpan.FromMinutes(5), stoppingToken); // repeat interval
         }
     }
 
@@ -42,5 +56,24 @@ public sealed class InterviewReminder : BackgroundService
         //{
         //    // implement what you want to do (e.g., prepare reminder, mark task, etc.)
         //}
+    }
+
+    private async Task RetryAsync(Func<Task> action, int maxRetries, TimeSpan retryDelay, CancellationToken ct)
+    {
+        int attempt = 0;
+        while (attempt < maxRetries)
+        {
+            try
+            {
+                await action();
+                return;
+            }
+            catch
+            {
+                attempt++;
+                if (attempt >= maxRetries) throw;
+                await Task.Delay(retryDelay, ct);
+            }
+        }
     }
 }
