2c4dee77aedd0894c02884ead0310c41b3a6baf0
zulhamzakaria <m.zulham.zakaria@gmail.com>
2025-12-30 19:20:38 +0800

    - RejectInterview (Task) action

---
+++ Projects/interview-system/InterviewSystem.Application/InterviewTasks/Evaluation/RejectInterview/RejectInterviewCommand.cs
@@ -0,0 +1,10 @@
+﻿using InterviewSystem.Domain.Common.ErrorHandling;
+using MediatR;
+
+namespace InterviewSystem.Application.InterviewTasks.Evaluation.RejectInterview;
+
+public sealed record RejectInterviewCommand(
+    Guid TaskId,
+    Guid AssigneeId,
+    string? Reason) : IRequest<Result<Guid>>;
+
