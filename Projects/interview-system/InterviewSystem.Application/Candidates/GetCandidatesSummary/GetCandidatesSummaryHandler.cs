3e825fd90639abb0662fd486f0bb9265dae22f28
zulhamzakaria <m.zulham.zakaria@gmail.com>
2026-01-01 21:48:29 +0800

    - debugging

--- Projects/interview-system/InterviewSystem.Application/Candidates/GetCandidatesSummary/GetCandidatesSummaryHandler.cs
+++ Projects/interview-system/InterviewSystem.Application/Candidates/GetCandidatesSummary/GetCandidatesSummaryHandler.cs
@@ -24,7 +24,7 @@ public sealed class GetCandidatesSummaryHandler : IRequestHandler<GetCandidatesS
 
         var filteredCandidates = candidates
             .Where(c => request.AppliedPosition == null || c.AppliedPosition == request.AppliedPosition)
-            .Where(c => request.SubmittedForInterview == null )
+            .Where(c => request.SubmittedForInterview == null || c.SubmittedForInterview == request.SubmittedForInterview)
             .ToList();
 
         if (filteredCandidates.Any() is false)
