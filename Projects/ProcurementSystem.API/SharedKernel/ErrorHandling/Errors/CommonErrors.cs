d396e29a27c6efb7dd34c2d48a72c3e6c225ffc1
zulhamzakaria <m.zulham.zakaria@gmail.com>
2026-01-12 15:30:41 +0800

    - CommonErrors library

---
+++ Projects/ProcurementSystem.API/SharedKernel/ErrorHandling/Errors/CommonErrors.cs
@@ -0,0 +1,15 @@
+﻿namespace ProcurementSystem.API.SharedKernel.ErrorHandling.Errors;
+
+public static class CommonErrors
+{
+    public static Error NotFound(string entityName, string identifier)
+        => new Error(
+            errorCode: "COMMON_NOT_FOUND",
+            message: $"{entityName} with identifier '{identifier}' was not found.",
+            errorType: Enums.ErrorType.NotFound);
+    public static Error InvalidInput(string details)
+        => new Error(
+            errorCode: "COMMON_INVALID_INPUT",
+            message: $"Invalid input: {details}",
+            errorType: Enums.ErrorType.Validation);
+}
