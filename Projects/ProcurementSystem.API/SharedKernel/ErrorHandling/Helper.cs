958da5c2708fbe30d4c66036196e9f37974fb1bb
zulhamzakaria <m.zulham.zakaria@gmail.com>
2026-01-12 15:44:41 +0800

    - for modifying someVar to SomeVar

---
+++ Projects/ProcurementSystem.API/SharedKernel/ErrorHandling/Helper.cs
@@ -0,0 +1,17 @@
+﻿namespace ProcurementSystem.API.SharedKernel.ErrorHandling;
+
+public static class Helper
+{
+    public static string Humanize(string input)
+    {
+        if (string.IsNullOrWhiteSpace(input)) return string.Empty;
+
+        var result = System.Text.RegularExpressions.Regex.Replace(
+            input,
+            "(\\B[A-Z])",
+            " $1"
+        );
+
+        return char.ToUpper(result[0]) + result.Substring(1);
+    }
+}
