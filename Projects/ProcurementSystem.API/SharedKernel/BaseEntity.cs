2ff89a7c0aaf91ba17c1999166e820c245bcb9ed
zulhamzakaria <m.zulham.zakaria@gmail.com>
2026-01-12 17:22:55 +0800

    - WIP

--- Projects/ProcurementSystem.API/SharedKernel/BaseEntity.cs
+++ Projects/ProcurementSystem.API/SharedKernel/BaseEntity.cs
@@ -4,7 +4,19 @@ public abstract class BaseEntity
 {
     public Guid Id { get; protected set; } = Guid.NewGuid();
     public Guid CreatedBy { get; protected set; }
-    public DateTimeOffset CreatedAt { get; protected set; } = DateTimeOffset.UtcNow;
-    public Guid UpdatedBy { get; protected set; }
-    public DateTimeOffset UpdatedAt { get; protected set; } = DateTimeOffset.UtcNow;
+    public DateTimeOffset CreatedAt { get; protected set; } 
+    public Guid? UpdatedBy { get; protected set; }
+    public DateTimeOffset? UpdatedAt { get; protected set; } 
+
+    protected BaseEntity(Guid createdBy)
+    {
+        CreatedBy = createdBy;
+        CreatedAt = DateTimeOffset.UtcNow;
+    }
+
+    public void SetUpdated(Guid updatedBy)
+    {
+        UpdatedBy = updatedBy;
+        UpdatedAt = DateTimeOffset.UtcNow;
+    }
 }
