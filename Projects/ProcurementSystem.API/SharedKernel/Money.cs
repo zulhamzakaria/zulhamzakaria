f546239c99ca8d05a3115f0256c4e38b70b829d7
zulhamzakaria <m.zulham.zakaria@gmail.com>
2026-01-12 15:52:16 +0800

    - WIP

--- Projects/ProcurementSystem.API/SharedKernel/Money.cs
+++ Projects/ProcurementSystem.API/SharedKernel/Money.cs
@@ -3,5 +3,16 @@
 public class Money
 {
     public decimal Amount { get; }
-    public string Currency { get; } = "MYR";
+    public string Currency { get; }
+
+    public Money(decimal amount, string currency)
+    {
+        if(Amount < 0)
+        {
+            throw new ArgumentException("Amount cannot be negative", nameof(amount));
+        }
+    }
+
+
+
 }
