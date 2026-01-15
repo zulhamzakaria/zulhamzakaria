4e07de549f3baf59ebebbcab026652ab4ec78a35
zulhamzakaria <m.zulham.zakaria@gmail.com>
2026-01-15 10:09:23 +0800

    - WIP

---
+++ Projects/ProcurementSystem.API/Modules/Procurement/Infrastructure/ProcurementDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProcurementSystem.API.Modules.Procurement.Domain.Aggregates;
+
+namespace ProcurementSystem.API.Modules.Procurement.Infrastructure;
+
+public class ProcurementDbContext : DbContext
+{
+    public DbSet<PurchaseRequest> PurchaseRequests => Set<PurchaseRequest>();
+    public ProcurementDbContext(DbContextOptions<ProcurementDbContext> options) : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        ////for external config file. scans the assembly for all 
+        ////IEntityTypeConfiguration implementations i.e IEntityTypeConfiguration<PurchaseRequest>
+        //modelBuilder.ApplyConfigurationsFromAssembly(typeof(ProcurementDbContext).Assembly);
+    }
+
+}
