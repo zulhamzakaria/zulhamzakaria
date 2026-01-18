c0f00294dc1258db57fcbe070ce109ff6da8b0f5
zulhamzakaria <m.zulham.zakaria@gmail.com>
2026-01-18 14:14:40 +0800

    - dbsets for PRItems and ITems

--- Projects/ProcurementSystem.API/Modules/Procurement/Infrastructure/ProcurementDbContext.cs
+++ Projects/ProcurementSystem.API/Modules/Procurement/Infrastructure/ProcurementDbContext.cs
@@ -1,11 +1,14 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using ProcurementSystem.API.Modules.Procurement.Domain.Aggregates;
+using ProcurementSystem.API.Modules.Procurement.Domain.Entities;
 
 namespace ProcurementSystem.API.Modules.Procurement.Infrastructure;
 
 public class ProcurementDbContext : DbContext
 {
     public DbSet<PurchaseRequest> PurchaseRequests => Set<PurchaseRequest>();
+    public DbSet<PurchaseRequestItem> PurchaseRequestItems => Set<PurchaseRequestItem>();
+    public DbSet<Item> Items => Set<Item>();
     public ProcurementDbContext(DbContextOptions<ProcurementDbContext> options) : base(options)
     {
     }
