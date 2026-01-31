6fc5476cad0db786e367b2b0403c24dbbf752a18
zulhamzakaria <m.zulham.zakaria@gmail.com>
2026-02-01 06:37:10 +0800

    - WIP

---
+++ Projects/ProcurementSystem.API/Modules/Procurement/Infrastructure/Repositories/IProcurementUnitOfWork.cs
@@ -0,0 +1,9 @@
+﻿namespace ProcurementSystem.API.Modules.Procurement.Infrastructure.Repositories;
+
+public interface IProcurementUnitOfWork
+{
+    IOrderRepository Orders { get; }
+    IProductRepository Products { get; }
+    ISupplierRepository Suppliers { get; }
+    Task<int> SaveChangesAsync(CancellationToken cancellationToken = default);
+}
