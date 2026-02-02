f0cd9adcb9b22d4ebc406c144dcb75dc314a1105
zulhamzakaria <m.zulham.zakaria@gmail.com>
2026-02-02 18:52:33 +0800

    - get tnenatId from TenantClaim

--- Projects/ProcurementSystem.API/Middleware/TenantResolutionMiddleware.cs
+++ Projects/ProcurementSystem.API/Middleware/TenantResolutionMiddleware.cs
@@ -1,4 +1,5 @@
 ﻿using ProcurementSystem.API.SharedKernel.Infrastructure;
+using ProcurementSystem.API.Tenancy;
 
 namespace ProcurementSystem.API.Middleware;
 
@@ -13,7 +14,8 @@ public sealed class TenantResolutionMiddleware
 
     public async Task InvokeAsync(HttpContext context, CurrentTenant currentTenant)
     {
-        var claim = context.User.Claims.FirstOrDefault(c => c.Type == "tenant_id");
+        //var claim = context.User.Claims.FirstOrDefault(c => c.Type == "tenant_id");
+        var claim = context.User.Claims.FirstOrDefault(c => c.Type == TenantClaim.TenantId);
         if(claim is null)
             throw new UnauthorizedAccessException("Tenant ID claim is missing.");
         if(!Guid.TryParse(claim.Value, out var tenantId))
