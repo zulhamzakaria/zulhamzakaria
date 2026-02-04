6ae3d03c33a43f08aa110e4aae7fc5be09ff2d70
zulhamzakaria <m.zulham.zakaria@gmail.com>
2026-02-04 10:32:41 +0800

    - unitofwork di repo

--- Projects/ProcurementSystem.API/Extensions/DICollectionExtensions.cs
+++ Projects/ProcurementSystem.API/Extensions/DICollectionExtensions.cs
@@ -1,5 +1,8 @@
-﻿using ProcurementSystem.API.Modules.Procurement.Infrastructure.Repositories;
+﻿using ProcurementSystem.API.Modules.IdentityAndAccess.Infrastructure;
+using ProcurementSystem.API.Modules.Procurement.Infrastructure;
+using ProcurementSystem.API.Modules.Procurement.Infrastructure.Repositories;
 using ProcurementSystem.API.SharedKernel.Infrastructure;
+using ProcurementSystem.API.SharedKernel.Infrastructure.Abstractions;
 using ProcurementSystem.API.SharedKernel.Security;
 
 namespace ProcurementSystem.API.Extensions;
@@ -11,6 +14,8 @@ public static class DICollectionExtensions
         services.AddScoped<IProcurementUnitOfWork, ProcurementUnitOfWork>();
         services.AddScoped<IPurchaseRequestRepository, PurchaseRequestRepository>();
         services.AddScoped<IPurchaseOrderRepository, PurchaseOrderRepository>();
+        services.AddScoped<IUnitOfWork, EfUnitOfWork<ProcurementDbContext>>();
+        services.AddScoped<IUnitOfWork, EfUnitOfWork<IADbContext>>();
         services.AddScoped<IJwtTokenGenerator, JwtTokenGenerator>();
         return services;
     }
