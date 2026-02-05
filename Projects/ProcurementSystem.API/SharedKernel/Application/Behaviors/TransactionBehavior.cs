d6741f9569a4d25c7a49546312662da2babf0f98
zulhamzakaria <m.zulham.zakaria@gmail.com>
2026-02-05 17:30:11 +0800

    - might not be usable considering there a split UoW for modules

--- Projects/ProcurementSystem.API/SharedKernel/Application/Behaviors/TransactionBehavior.cs
+++ Projects/ProcurementSystem.API/SharedKernel/Application/Behaviors/TransactionBehavior.cs
@@ -1,5 +1,25 @@
-﻿namespace ProcurementSystem.API.SharedKernel.Application.Behaviors;
+﻿using ProcurementSystem.API.SharedKernel.Application.Messaging;
+using ProcurementSystem.API.SharedKernel.Infrastructure.Abstractions;
 
-public class TransactionBehavior
+namespace ProcurementSystem.API.SharedKernel.Application.Behaviors;
+
+public sealed class TransactionBehavior<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse>
+    where TRequest : IRequest<TResponse>
 {
+    private readonly IUnitOfWork _unitOfWork;
+
+    public TransactionBehavior(IUnitOfWork unitOfWork)
+    {
+        _unitOfWork = unitOfWork;
+    }
+
+    public async Task<TResponse> Handle
+        (TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken = default)
+    {
+        var response = await next();
+
+        await _unitOfWork.SaveChangesAsync(cancellationToken);
+
+        return response;
+    }
 }
