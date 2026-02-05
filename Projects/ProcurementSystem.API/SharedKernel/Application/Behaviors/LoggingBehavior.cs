9caad27737ede9e6b4687589336af28a0007671d
zulhamzakaria <m.zulham.zakaria@gmail.com>
2026-02-05 17:21:10 +0800

    - mediatr logger

--- Projects/ProcurementSystem.API/SharedKernel/Application/Behaviors/LoggingBehavior.cs
+++ Projects/ProcurementSystem.API/SharedKernel/Application/Behaviors/LoggingBehavior.cs
@@ -1,5 +1,36 @@
-﻿namespace ProcurementSystem.API.SharedKernel.Application.Behaviors;
+﻿using ProcurementSystem.API.SharedKernel.Application.Messaging;
 
-public class LoggingBehavior
+namespace ProcurementSystem.API.SharedKernel.Application.Behaviors;
+
+public sealed class LoggingBehavior<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse>
+    where TRequest : IRequest<TResponse>
 {
+    private readonly ILogger<LoggingBehavior<TRequest, TResponse>> _logger;
+
+    public LoggingBehavior(ILogger<LoggingBehavior<TRequest, TResponse>> logger)
+    {
+        _logger = logger;
+    }
+
+    public async Task<TResponse> Handle
+        (TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken = default)
+    {
+        var requestName = typeof(TRequest).Name;
+
+        _logger.LogInformation("Handling {RequestName}", requestName);
+
+        try
+        {
+            var response = await next();
+
+            _logger.LogInformation("Handled {RequestName} successfully", requestName);
+
+            return response;
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, "Error handling {RequestName}", requestName);
+            throw;
+        }
+    }
 }
