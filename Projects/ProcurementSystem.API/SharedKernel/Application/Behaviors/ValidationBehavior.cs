96c8068d11f77d187c8fdc43b5bf3dd572dbf442
zulhamzakaria <m.zulham.zakaria@gmail.com>
2026-02-05 17:25:28 +0800

    - requests validator for mediatr - WIP

--- Projects/ProcurementSystem.API/SharedKernel/Application/Behaviors/ValidationBehavior.cs
+++ Projects/ProcurementSystem.API/SharedKernel/Application/Behaviors/ValidationBehavior.cs
@@ -1,5 +1,41 @@
-﻿namespace ProcurementSystem.API.SharedKernel.Application.Behaviors;
+﻿using FluentValidation;
+using ProcurementSystem.API.SharedKernel.Application.Messaging;
 
-public class ValidationBehavior
+namespace ProcurementSystem.API.SharedKernel.Application.Behaviors;
+
+public sealed class ValidationBehavior<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse>
+    where TRequest : IRequest<TResponse>
 {
+    private readonly IEnumerable<IValidator<TRequest>> _validators;
+
+    public ValidationBehavior(IEnumerable<IValidator<TRequest>> validators)
+    {
+        _validators = validators;
+    }
+
+    public async Task<TResponse> Handle
+        (TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken = default)
+    {
+        if (!_validators.Any())
+        {
+            return await next();
+        }
+
+        var context = new ValidationContext<TRequest>(request);
+
+        var validationResults = await Task.WhenAll(
+            _validators.Select(v => v.ValidateAsync(context, cancellationToken)));
+
+        var failures = validationResults
+            .SelectMany(r => r.Errors)
+            .Where(f => f != null)
+            .ToList();
+
+        if (failures.Count != 0)
+        {
+            throw new ValidationException(failures);
+        }
+
+        return await next();
+    }
 }
