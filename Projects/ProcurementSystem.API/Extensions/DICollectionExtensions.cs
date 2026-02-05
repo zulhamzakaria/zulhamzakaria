3e7ce1e4336d99f05f0984fb6b9ae4837a6e293f
zulhamzakaria <m.zulham.zakaria@gmail.com>
2026-02-05 17:46:53 +0800

    - DI registration

--- Projects/ProcurementSystem.API/Extensions/DICollectionExtensions.cs
+++ Projects/ProcurementSystem.API/Extensions/DICollectionExtensions.cs
@@ -2,8 +2,11 @@
 using ProcurementSystem.API.Modules.IdentityAndAccess.Infrastructure.Repositories;
 using ProcurementSystem.API.Modules.Procurement.Infrastructure;
 using ProcurementSystem.API.Modules.Procurement.Infrastructure.Repositories;
+using ProcurementSystem.API.SharedKernel.Application.Behaviors;
+using ProcurementSystem.API.SharedKernel.Application.Messaging;
 using ProcurementSystem.API.SharedKernel.Infrastructure;
 using ProcurementSystem.API.SharedKernel.Infrastructure.Abstractions;
+using ProcurementSystem.API.SharedKernel.Infrastructure.Messaging;
 using ProcurementSystem.API.SharedKernel.Security;
 
 namespace ProcurementSystem.API.Extensions;
@@ -19,6 +22,9 @@ public static class DICollectionExtensions
         services.AddScoped<IUnitOfWork, EfUnitOfWork<IADbContext>>();
         services.AddScoped<IJwtTokenGenerator, JwtTokenGenerator>();
         services.AddScoped<ITenantRepository, TenantRepository>();
+        services.AddScoped<IMediator, Mediator>();
+        services.AddScoped(typeof(IPipelineBehavior<,>), typeof(LoggingBehavior<,>));
+        services.AddScoped(typeof(IPipelineBehavior<,>), typeof(ValidationBehavior<,>));
         return services;
     }
 }
