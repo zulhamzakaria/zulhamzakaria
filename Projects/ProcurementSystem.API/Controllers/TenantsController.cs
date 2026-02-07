5cbbd4a4e4d5bfa0595660f2a6d392c0a1e16357
zulhamzakaria <m.zulham.zakaria@gmail.com>
2026-02-07 12:30:49 +0800

    - controller

---
+++ Projects/ProcurementSystem.API/Controllers/TenantsController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProcurementSystem.API.Modules.IdentityAndAccess.Application.Commands.CreateTenant;
+using ProcurementSystem.API.SharedKernel.Application.Messaging;
+using ProcurementSystem.API.SharedKernel.ErrorHandling;
+
+namespace ProcurementSystem.API.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class TenantsController : ControllerBase
+{
+    private readonly IMediator _mediator;
+    public TenantsController(IMediator mediator)
+    {
+        _mediator = mediator;
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> CreateTenant
+        ([FromBody] CreateTenantRequest request, CancellationToken cancellationToken)
+    {
+        var command = new CreateTenantCommand
+            (request.TenantName, request.TenantAlias);
+
+        var result = await _mediator.Send<CreateTenantCommand, Result<Guid>>
+            (command, cancellationToken);
+        if (result.IsFailure)
+        {
+            return BadRequest(result.Errors);
+        }
+        return CreatedAtAction(nameof(CreateTenant), new { id = result.Value }, null);
+    }
+}
---
+++ Projects/ProcurementSystem.API/Modules/IdentityAndAccess/Application/Commands/CreateTenant/CreateTenantRequest.cs
@@ -0,0 +1,3 @@
+﻿namespace ProcurementSystem.API.Modules.IdentityAndAccess.Application.Commands.CreateTenant;
+
+public sealed record CreateTenantRequest(string TenantName, string TenantAlias);
