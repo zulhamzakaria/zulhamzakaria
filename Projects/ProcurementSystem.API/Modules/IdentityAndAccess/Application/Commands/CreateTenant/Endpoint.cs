cfaa4682933a766d48b804b0b61eff6b3d16af85
zulhamzakaria <m.zulham.zakaria@gmail.com>
2026-02-13 10:21:41 +0800

    - moved the controller inside the module CreateTenant action

---
+++ Projects/ProcurementSystem.API/Modules/IdentityAndAccess/Application/Commands/CreateTenant/Endpoint.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProcurementSystem.API.SharedKernel.Application.Messaging;
+using ProcurementSystem.API.SharedKernel.ErrorHandling;
+
+namespace ProcurementSystem.API.Modules.IdentityAndAccess.Application.Commands.CreateTenant
+{
+    [Route("api/tenants")]
+    [ApiController]
+    public class Endpoint : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public Endpoint(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateTenant
+            ([FromBody] CreateTenantRequest request, CancellationToken ct)
+        {
+            var command = new CreateTenantCommand
+                (request.TenantAlias,request.TenantName);
+
+            var result = await _mediator.Send<CreateTenantCommand, Result<Guid>>
+                (command, ct);
+
+            if(result.IsFailure)
+                return BadRequest(result.Errors);
+
+            return Ok(result.Value);
+        }
+
+    }
+}
