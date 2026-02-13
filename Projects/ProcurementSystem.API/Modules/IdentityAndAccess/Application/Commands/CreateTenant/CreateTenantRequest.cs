48cd79e4457f28eed41ee7a94c753e58010d5c9d
zulhamzakaria <m.zulham.zakaria@gmail.com>
2026-02-13 10:27:25 +0800

    - annotations

--- Projects/ProcurementSystem.API/Modules/IdentityAndAccess/Application/Commands/CreateTenant/CreateTenantRequest.cs
+++ Projects/ProcurementSystem.API/Modules/IdentityAndAccess/Application/Commands/CreateTenant/CreateTenantRequest.cs
@@ -1,3 +1,7 @@
-﻿namespace ProcurementSystem.API.Modules.IdentityAndAccess.Application.Commands.CreateTenant;
+﻿using System.ComponentModel.DataAnnotations;
 
-public sealed record CreateTenantRequest(string TenantName, string TenantAlias);
+namespace ProcurementSystem.API.Modules.IdentityAndAccess.Application.Commands.CreateTenant;
+
+public sealed record CreateTenantRequest(
+    [Required, StringLength(50, MinimumLength = 3)] string TenantName,
+    [Required, StringLength(50, MinimumLength = 3)] string TenantAlias);
