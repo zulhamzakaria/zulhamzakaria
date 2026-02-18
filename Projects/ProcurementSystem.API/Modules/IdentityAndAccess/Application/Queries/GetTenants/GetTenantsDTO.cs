1fd77051dff28fe12dbbe79d1c17b5c6a18d2634
zulhamzakaria <m.zulham.zakaria@gmail.com>
2026-02-18 19:29:17 +0800

    - WIP

---
+++ Projects/ProcurementSystem.API/Modules/IdentityAndAccess/Application/Queries/GetTenants/GetTenantsDTO.cs
@@ -0,0 +1,4 @@
+﻿namespace ProcurementSystem.API.Modules.IdentityAndAccess.Application.Queries.GetTenants;
+
+public sealed record GetTenantsDTO
+    (Guid Id, string TenantName, string TenantAlias);
---
+++ Projects/ProcurementSystem.API/Modules/IdentityAndAccess/Application/Queries/GetTenants/GetTenantsQuery.cs
@@ -0,0 +1,7 @@
+﻿using ProcurementSystem.API.SharedKernel.Application.Messaging;
+using ProcurementSystem.API.SharedKernel.ErrorHandling;
+
+namespace ProcurementSystem.API.Modules.IdentityAndAccess.Application.Queries.GetTenants;
+
+public sealed record GetTenantsQuery : IRequest<Result<IReadOnlyCollection<GetTenantsDTO>>>;
+
