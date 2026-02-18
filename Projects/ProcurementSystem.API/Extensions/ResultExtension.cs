bca0d6dbcd982c59ddfba5b77cad608f641ca6d2
zulhamzakaria <m.zulham.zakaria@gmail.com>
2026-02-18 18:54:23 +0800

    - removed details so only errors show the issues

--- Projects/ProcurementSystem.API/Extensions/ResultExtension.cs
+++ Projects/ProcurementSystem.API/Extensions/ResultExtension.cs
@@ -28,7 +28,6 @@ public static class ResultExtension
         {
             Title = "Request Failed",
             Status = statusCode,
-            Detail = string.Join("; ", errors.Select(errors => errors.Message))
         };
 
         problems.Extensions["errors"] = errors;
