cf191c15d62ba84a78e186f6e27f53e3db74b6aa
zulhamzakaria <m.zulham.zakaria@gmail.com>
2026-02-24 18:39:13 +0800

    - placeholder

---
+++ Projects/ProcurementSystem.API/Modules/IdentityAndAccess/Application/Commands/SignInUser/SignInUserCommand.cs
@@ -0,0 +1,7 @@
+﻿using ProcurementSystem.API.SharedKernel.Application.Messaging;
+
+namespace ProcurementSystem.API.Modules.IdentityAndAccess.Application.Commands.SignInUser;
+
+public sealed record SignInUserCommand
+    (string tenantAlias, string username, string password)
+    : IRequest<object>;
