54873bc2a72672b7cf9a142e103932948310bb28
zulhamzakaria <m.zulham.zakaria@gmail.com>
2026-02-25 13:19:00 +0800

    - WIP

---
+++ Projects/ProcurementSystem.API/Modules/IdentityAndAccess/Application/Commands/SignInUser/SignInUserEndpoint.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProcurementSystem.API.SharedKernel.Application.Messaging;
+
+namespace ProcurementSystem.API.Modules.IdentityAndAccess.Application.Commands.SignInUser;
+
+[Route("api/users")]
+[ApiController]
+public class SignInUserEndpoint : ControllerBase
+{
+    private readonly IMediator _mediator;
+    private readonly ILogger<SignInUserEndpoint> _logger;
+    public SignInUserEndpoint
+        (IMediator mediator, ILogger<SignInUserEndpoint> logger)
+    {
+        _mediator = mediator;
+        _logger = logger;
+    }
+}
