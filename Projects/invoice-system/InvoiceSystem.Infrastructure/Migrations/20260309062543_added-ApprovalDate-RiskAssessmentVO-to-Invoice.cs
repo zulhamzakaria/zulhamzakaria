0fe1a03bb6af6ca9a4e20c9f4d69e1a62028ec14
zulhamzakaria <m.zulham.zakaria@gmail.com>
2026-03-09 14:26:19 +0800

    - migration

---
+++ Projects/invoice-system/InvoiceSystem.Infrastructure/Migrations/20260309062543_added-ApprovalDate-RiskAssessmentVO-to-Invoice.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace InvoiceSystem.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class addedApprovalDateRiskAssessmentVOtoInvoice : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTimeOffset>(
+                name: "ApprovalDate",
+                table: "Invoices",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValue: new DateTimeOffset(new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, 0, 0, 0, 0)));
+
+            migrationBuilder.AddColumn<string>(
+                name: "RiskAssessment",
+                table: "Invoices",
+                type: "jsonb",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ApprovalDate",
+                table: "Invoices");
+
+            migrationBuilder.DropColumn(
+                name: "RiskAssessment",
+                table: "Invoices");
+        }
+    }
+}
