140bf26e18721f48a3ac43bd0c83ff0ecbf2d9a5
zulhamzakaria <m.zulham.zakaria@gmail.com>
2026-03-11 17:22:17 +0800

    - formatting

--- Projects/invoice-system/InvoiceSystem.MLTrainer/Trainers/RiskModelTrainer.cs
+++ Projects/invoice-system/InvoiceSystem.MLTrainer/Trainers/RiskModelTrainer.cs
@@ -1,12 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace InvoiceSystem.MLTrainer.Trainers;
 
-namespace InvoiceSystem.MLTrainer.Trainers
+internal class RiskModelTrainer
 {
-    internal class RiskModelTrainer
-    {
-    }
 }
