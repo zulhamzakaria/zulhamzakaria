4773587ac64d581f3a37324b2b36ab91ac7f8e29
zulhamzakaria <m.zulham.zakaria@gmail.com>
2026-03-13 19:07:09 +0800

    - wtire to csv

--- Projects/invoice-system/InvoiceSystem.Application/obj/Debug/net8.0/InvoiceSystem.Application.AssemblyInfo.cs
+++ Projects/invoice-system/InvoiceSystem.Application/obj/Debug/net8.0/InvoiceSystem.Application.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("InvoiceSystem.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+39e59096c1243f224a39b49ea2a1935941348045")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+48235c6b8033355f76dc157dbdfefbaaf1eeb13c")]
 [assembly: System.Reflection.AssemblyProductAttribute("InvoiceSystem.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("InvoiceSystem.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
